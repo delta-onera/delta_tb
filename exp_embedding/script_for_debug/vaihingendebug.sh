7457334fde2279f493095abb3a9c95097672452e
teletravail <adrienchanhonton@gmail.com>
2020-10-06 09:12:43 +0200

    ++

--- exp_embedding/script_for_debug/vaihingendebug.sh
+++ exp_embedding/script_for_debug/vaihingendebug.sh
@@ -1,7 +1,8 @@
+rm -r ../__pycache__
 rm -r build
 mkdir build
 
 /data/anaconda3/bin/python -u vaihingendebugtrain.py | tee build/trainlog.txt
 /data/anaconda3/bin/python -u vaihingendebugtest.py  | tee build/testlog.txt
 
-rm -r __pycache__
+rm -r ../__pycache__
