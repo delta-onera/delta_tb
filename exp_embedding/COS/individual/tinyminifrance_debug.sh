65764766136d846c997be89ac90323b3ecb03d45
Adrien Chan Hon Tong <achanhon@calculon.onera>
2020-10-14 15:24:49 +0200

    ?

---
+++ exp_embedding/COS/individual/tinyminifrance_debug.sh
@@ -0,0 +1,11 @@
+rm -r ../../__pycache__
+rm -r build
+mkdir build
+
+for town in  'Marseille_Martigues'
+do
+    /home/jcastillo/anaconda3/bin/python -u tinyminifrance_train.py $town | tee build/tinyminifrance_train$town.txt
+    /home/jcastillo/anaconda3/bin/python -u tinyminifrance_test.py $town | tee build/tinyminifrance_test$town.txt
+done
+
+rm -r ../../__pycache__
