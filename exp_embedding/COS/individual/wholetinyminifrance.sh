dac44d53b497df561c532d723317069ffa15927d
Adrien Chan Hon Tong <achanhon@calculon.onera>
2020-11-26 13:23:52 +0100

    ++

--- exp_embedding/COS/individual/wholetinyminifrance.sh
+++ exp_embedding/COS/individual/wholetinyminifrance.sh
@@ -2,13 +2,13 @@ rm -r ../../__pycache__
 rm -r build
 mkdir build
 
-/home/jcastillo/anaconda3/bin/python -u wholetinyminifrance_train.py normalize | tee build/wholetinyminifrancenormalize_train.txt
-/home/jcastillo/anaconda3/bin/python -u wholetinyminifrance_test.py normalize | tee build/wholetinyminifrancenormalize_test.txt
+CUDA_VISIBLE_DEVICES=2 /home/jcastillo/anaconda3/bin/python -u wholetinyminifrance_train.py normalize | tee build/wholetinyminifrancenormalize_train.txt
+CUDA_VISIBLE_DEVICES=2 /home/jcastillo/anaconda3/bin/python -u wholetinyminifrance_test.py normalize | tee build/wholetinyminifrancenormalize_test.txt
 
-/home/jcastillo/anaconda3/bin/python -u wholetinyminifrance_train.py | tee build/wholetinyminifrance_train.txt
-/home/jcastillo/anaconda3/bin/python -u wholetinyminifrance_test.py | tee build/wholetinyminifrance_test.txt
+CUDA_VISIBLE_DEVICES=2 /home/jcastillo/anaconda3/bin/python -u wholetinyminifrance_train.py | tee build/wholetinyminifrance_train.txt
+CUDA_VISIBLE_DEVICES=2 /home/jcastillo/anaconda3/bin/python -u wholetinyminifrance_test.py | tee build/wholetinyminifrance_test.txt
 
-/home/jcastillo/anaconda3/bin/python -u wholetinyminifrance_train.py grey | tee build/wholetinyminifrancegrey_train.txt
-/home/jcastillo/anaconda3/bin/python -u wholetinyminifrance_test.py grey | tee build/wholetinyminifrancegrey_test.txt
+CUDA_VISIBLE_DEVICES=2 /home/jcastillo/anaconda3/bin/python -u wholetinyminifrance_train.py grey | tee build/wholetinyminifrancegrey_train.txt
+CUDA_VISIBLE_DEVICES=2 /home/jcastillo/anaconda3/bin/python -u wholetinyminifrance_test.py grey | tee build/wholetinyminifrancegrey_test.txt
 
 rm -r ../../__pycache__
