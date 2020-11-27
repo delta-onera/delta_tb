2da9cb7af1ec51a1ce1aa2ecc713a5951fb47b1e
teletravail <adrienchanhonton@gmail.com>
2020-11-27 16:04:45 +0100

    +

--- exp_embedding/baseline/go.sh
+++ exp_embedding/baseline/go.sh
@@ -2,7 +2,13 @@ rm -r ../../__pycache__
 rm -r build
 mkdir build
 
-/data/anaconda3/bin/python -u vaihingen_train.py | tee build/vaihingen_train.txt
-/data/anaconda3/bin/python -u vaihingen_test.py  | tee build/vaihingen_test.txt
+/data/anaconda3/bin/python -u train.py VAIHINGEN | tee build/vaihingen_train.txt
+/data/anaconda3/bin/python -u test.py  VAIHINGEN | tee build/vaihingen_test.txt
+
+/data/anaconda3/bin/python -u train.py POTSDAM | tee build/vaihingen_train.txt
+/data/anaconda3/bin/python -u test.py  POTSDAM | tee build/vaihingen_test.txt
+
+/data/anaconda3/bin/python -u train.py BRUGES | tee build/vaihingen_train.txt
+/data/anaconda3/bin/python -u test.py  BRUGES | tee build/vaihingen_test.txt
 
 rm -r ../../__pycache__
