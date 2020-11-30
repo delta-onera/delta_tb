aa0259638f2b75fc02570838efeac424f2b73991
teletravail <adrienchanhonton@gmail.com>
2020-11-30 09:17:48 +0100

    +++++

--- exp_embedding/LOD0/embedding/old/doubledebug.sh
+++
@@ -1,8 +0,0 @@
-rm -r ../__pycache__
-rm -r build
-mkdir build
-
-/data/anaconda3/bin/python -u doubledebugtrain.py | tee build/trainlog.txt
-/data/anaconda3/bin/python -u doubledebugtest.py  | tee build/testlog.txt
-
-rm -r ../__pycache__
