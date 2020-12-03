19bca1da307df1e8139010ed719281b19865aa4d
teletravail <adrienchanhonton@gmail.com>
2020-12-03 09:56:46 +0100

    +

--- exp_embedding/singletrain/handmade_embedding/go.sh
+++ exp_embedding/singletrain/handmade_embedding/go.sh
@@ -1,19 +1,11 @@
-rm -r ../__pycache__
-rm -r __pycache__
+rm -r ../../__pycache__
 rm -r build
 mkdir build
 
-/home/achanhon/anaconda3/envs/rahh/bin/python -u train.py TOULOUSE | tee build/toulouse_gray_train.txt
-/home/achanhon/anaconda3/envs/rahh/bin/python -u test.py  TOULOUSE | tee build/toulouse_gray_test.txt
+/home/achanhon/anaconda3/bin/python -u train.py VAIHINGEN BRUGES POTSDAM | tee build/vbp2vbp_train.txt
+/home/achanhon/anaconda3/bin/python -u test.py  VAIHINGEN BRUGES POTSDAM | tee build/vbp2vbp_test.txt
 
-/home/achanhon/anaconda3/envs/rahh/bin/python -u train.py VAIHINGEN | tee build/vaihingen_train.txt
-/home/achanhon/anaconda3/envs/rahh/bin/python -u test.py  VAIHINGEN | tee build/vaihingen_test.txt
+/home/achanhon/anaconda3/bin/python -u train.py VAIHINGEN_all BRUGES_all | tee build/vb2p_train.txt
+/home/achanhon/anaconda3/bin/python -u test.py  POTSDAM_all | tee build/vb2p_test.txt
 
-/home/achanhon/anaconda3/envs/rahh/bin/python -u train.py POTSDAM | tee build/vaihingen_train.txt
-/home/achanhon/anaconda3/envs/rahh/bin/python -u test.py  POTSDAM | tee build/vaihingen_test.txt
-
-/home/achanhon/anaconda3/envs/rahh/bin/python -u train.py BRUGES | tee build/vaihingen_train.txt
-/home/achanhon/anaconda3/envs/rahh/bin/python -u test.py  BRUGES | tee build/vaihingen_test.txt
-
-rm -r ../__pycache__
-rm -r __pycache__
+rm -r ../../__pycache__
