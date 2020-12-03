2df409291a9f7a7cf3b54409785a64baccea5cd4
achanhon <adrien.chan_hon_tong@onera.fr>
2020-12-03 12:42:53 +0100

    +

--- exp_embedding/singletrain/handmade_embedding/go.sh
+++ exp_embedding/singletrain/handmade_embedding/go.sh
@@ -2,10 +2,10 @@ rm -r ../../__pycache__
 rm -r build
 mkdir build
 
-/home/achanhon/anaconda3/bin/python -u train.py VAIHINGEN BRUGES POTSDAM | tee build/vbp2vbp_train.txt
-/home/achanhon/anaconda3/bin/python -u test.py  VAIHINGEN BRUGES POTSDAM | tee build/vbp2vbp_test.txt
+/data/anaconda3/bin/python -u train.py VAIHINGEN BRUGES POTSDAM | tee build/vbp2vbp_train.txt
+/data/anaconda3/bin/python -u test.py  VAIHINGEN BRUGES POTSDAM | tee build/vbp2vbp_test.txt
 
-/home/achanhon/anaconda3/bin/python -u train.py VAIHINGEN_all BRUGES_all | tee build/vb2p_train.txt
-/home/achanhon/anaconda3/bin/python -u test.py  POTSDAM_all | tee build/vb2p_test.txt
+/data/anaconda3/bin/python -u train.py VAIHINGEN_all BRUGES_all | tee build/vb2p_train.txt
+/data/anaconda3/bin/python -u test.py  POTSDAM_all | tee build/vb2p_test.txt
 
 rm -r ../../__pycache__
