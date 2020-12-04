ee439f87ec8c6b39ec15c998e7b25d6e007cd490
achanhon <adrien.chan_hon_tong@onera.fr>
2020-12-04 16:15:45 +0100

    rahh

--- exp_embedding/singletrain/baseline/go.sh
+++ exp_embedding/singletrain/baseline/go.sh
@@ -4,11 +4,11 @@ mkdir build
 
 echo "############################ different training settings -- all test on potsdam ############################"
 echo "supervised color"
-/achanhon/anaconda3/bin/python -u train.py POTSDAM_lod0
+/data/anaconda3/bin/python -u train.py POTSDAM_lod0
 /data/anaconda3/bin/python -u test.py  POTSDAM_lod0 | tee build/p2pcolor.txt
 
 echo "supervised"
-/achanhon/anaconda3/bin/python -u train.py POTSDAM_lod0 normalize
+/data/anaconda3/bin/python -u train.py POTSDAM_lod0 normalize
 /data/anaconda3/bin/python -u test.py  POTSDAM_lod0 normalize | tee build/p2p.txt
 
 echo "transfert "
