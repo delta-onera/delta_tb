e201a9221b84845f864229f5872bfb3a9286df9d
teletravail <adrienchanhonton@gmail.com>
2020-12-07 15:40:39 +0100

    ++

--- exp_embedding/singletrain/embedding_color/go.sh
+++ exp_embedding/singletrain/embedding_color/go.sh
@@ -6,14 +6,7 @@ echo "semi-supervised embedding color"
 /data/anaconda3/bin/python -u train_sum.py AIRS VAIHINGEN BRUGES POTSDAM
 /data/anaconda3/bin/python -u test.py  POTSDAM | tee build/avbp2p_color_sum.txt
 
-/data/anaconda3/bin/python -u train_sum.py AIRS* VAIHINGEN* BRUGES* POTSDAM
-/data/anaconda3/bin/python -u test.py  POTSDAM | tee build/aavvbbp2p_color_sum.txt
-
 /data/anaconda3/bin/python -u train.py AIRS VAIHINGEN BRUGES POTSDAM
 /data/anaconda3/bin/python -u test.py  POTSDAM | tee build/avbp2p_color_graph.txt
-
-/data/anaconda3/bin/python -u train.py AIRS* VAIHINGEN* BRUGES* POTSDAM
-/data/anaconda3/bin/python -u test.py  POTSDAM | tee build/aavvbbp2p_color_graph.txt
-
 
 rm -r ../../__pycache__
--- exp_embedding/singletrain/handmade_embedding/go.sh
+++ exp_embedding/singletrain/handmade_embedding/go.sh
@@ -3,21 +3,12 @@ rm -r build
 mkdir build
 
 echo "transfert"
-/data/anaconda3/bin/python -u train.py AIRS*
-/data/anaconda3/bin/python -u test.py  POTSDAM | tee build/aa2p.txt
-
-/data/anaconda3/bin/python -u train.py VAIHINGEN*
-/data/anaconda3/bin/python -u test.py  POTSDAM | tee build/vv2p.txt
-
-/data/anaconda3/bin/python -u train.py AIRS* VAIHINGEN* BRUGES*
+/data/anaconda3/bin/python -u train.py AIRS VAIHINGEN BRUGES
 /data/anaconda3/bin/python -u test.py  POTSDAM | tee build/vvbbaa2p.txt
 
 echo "semi-supervised"
 /data/anaconda3/bin/python -u train.py AIRS VAIHINGEN BRUGES POTSDAM
 /data/anaconda3/bin/python -u test.py  POTSDAM | tee build/avbp2p.txt
-
-/data/anaconda3/bin/python -u train.py AIRS* VAIHINGEN* BRUGES* POTSDAM
-/data/anaconda3/bin/python -u test.py  POTSDAM | tee build/aavvbbp2p.txt
 
 
 rm -r ../../__pycache__
