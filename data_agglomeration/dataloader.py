9fb26ab39ae1c9431ac7d8748ab449d56bbdaeba
teletravail <adrienchanhonton@gmail.com>
2021-04-07 11:29:11 +0200

    faster test ??

--- data_agglomeration/dataloader.py
+++ data_agglomeration/dataloader.py
@@ -221,6 +221,25 @@ class MiniWorld:
 
 
 def largeforward(net, image, device, tilesize=128, stride=32):
+    ## assume GPU is large enough -- use largeforwardCPU on very large image
+    net.eval()
+    with torch.no_grad():
+        pred = torch.zeros(1, 2, image.shape[2], image.shape[3]).to(device)
+        image = image.float().to(device)
+        i = 0
+        for row in range(0, image.shape[2] - tilesize + 1, stride):
+            for col in range(0, image.shape[3] - tilesize + 1, stride):
+                tmp = net(image[:, :, row : row + tilesize, col : col + tilesize])
+                pred[0, :, row : row + tilesize, col : col + tilesize] += tmp[0]
+
+                if i % 500 == 499:
+                    print("forward in progress", row, image.shape[2])
+                i += 1
+
+    return pred
+
+
+def largeforwardCPU(net, image, device, tilesize=128, stride=32):
     pred = torch.zeros(1, 2, image.shape[2], image.shape[3]).cpu()
 
     net.eval()
