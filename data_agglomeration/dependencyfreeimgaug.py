475529fc5681517cc4d49f5d9959fa2a8495acbe
teletravail <adrienchanhonton@gmail.com>
2021-04-13 22:16:44 +0200

    rahhhhhh

--- data_agglomeration/dependencyfreeimgaug.py
+++ data_agglomeration/dependencyfreeimgaug.py
@@ -9,71 +9,56 @@ def ensureimage(x, device="cuda"):
     )
 
 
-def pepperandsalt(k, x, nbpixel=30.0, device="cuda"):
-    row = np.random.randint(0, x.shape[2], size=nbpixel)
-    col = np.random.randint(0, x.shape[3], size=nbpixel)
-    pepperorsalt = np.random.randint(0, 2, size=nbpixel)
-    for j in range(3):
-        for i in range(nbpixel):
-            x[k, j, row[i], col[i]] = torch.ones(1).to(device) * float(pepperorsalt[i])
-
-
-def shift(k, x, level=30.0, device="cuda"):
-    for j in range(3):
-        x[k, j, :, :] += ((torch.rand() * 2.0 - 1.0) * level / 255.0).to(device)
-
-
-def gaussian(k, x, level=5.0, device="cuda"):
-    x[k] += (torch.randn(x[k].shape) * level / 255.0).to(device)
-
-
-def uniform(k, x, level=10.0, device="cuda"):
-    x[k] += ((torch.rand(x[k].shape) * 2.0 - 1.0) * level / 255.0).to(device)
-
-
-def cutout(k, x, level=5, device="cuda"):
-    row = random.randint(0, x.shape[2] - level - 1)
-    col = random.randint(0, x.shape[3] - level - 1)
-    x[k, :, row : row + level, col : col + level] = float(random.randint(0, 1))
-
-
-def dropchannel(k, x, device="cuda"):
-    x[k, random.randint(0, 2), :, :] = float(random.randint(0, 1))
-
-
-def gray(k, x, device="cuda"):
-    tmp = 0.333 * (x[k, 0, :, :] + x[k, 1, :, :] + x[k, 2, :, :])
-    x[k, 0, :, :] = tmp
-    x[k, 1, :, :] = tmp
-    x[k, 2, :, :] = tmp
-
-
 def augment(x, device="cuda"):
     with torch.no_grad():
         for i in range(x.shape[0] // 2):
             if random.randint(0, 10) == 0:
                 # large data augment
                 if random.randint(0, 1) == 0:
-                    dropchannel(i, x, device=device)
+                    x[i, random.randint(0, 2), :, :] = float(random.randint(0, 1))
                 else:
-                    gray(i, x, device=device)
+                    tmp = 0.333 * (x[i, 0, :, :] + x[i, 1, :, :] + x[i, 2, :, :])
+                    x[i, 0, :, :] = tmp
+                    x[i, 1, :, :] = tmp
+                    x[i, 2, :, :] = tmp
             else:
                 # small data augment
                 j = random.randint(0, 5)
                 if j == 0:
-                    pepperandsalt(i, x, device=device)
+                    level = 30
+                    row = np.random.randint(0, x.shape[2], size=level)
+                    col = np.random.randint(0, x.shape[3], size=level)
+                    pepperorsalt = np.random.randint(0, 2, size=level)
+                    for j in range(3):
+                        for i in range(level):
+                            x[i, j, row[i], col[i]] = (
+                                torch.ones(1) * float(pepperorsalt[i])
+                            ).to(device)
                 if j == 1:
-                    shift(i, x, device=device)
+                    level = 30.0
+                    for j in range(3):
+                        x[i, j, :, :] += (
+                            (torch.rand() * 2.0 - 1.0) * level / 255.0
+                        ).to(device)
                 if j == 2:
                     x[i : i + 1] = torch.nn.functional.max_pool2d(
                         x[i : i + 1], kernel_size=7, padding=3, stride=1
                     )
                 if j == 3:
-                    gaussian(i, x, device=device)
+                    level = 15.0
+                    x[i] += (torch.randn(x[i].shape) * level / 255.0).to(device)
                 if j == 4:
-                    uniform(i, x, device=device)
+                    level = 15.0
+                    x[i] += ((torch.rand(x[i].shape) * 2.0 - 1.0) * level / 255.0).to(
+                        device
+                    )
                 if j == 5:
-                    cutout(i, x, device=device)
+                    level = 5
+                    row = random.randint(0, x.shape[2] - level - 1)
+                    col = random.randint(0, x.shape[3] - level - 1)
+                    x[i, :, row : row + level, col : col + level] = float(
+                        random.randint(0, 1)
+                    )
 
-    x = torch.Tensor(x.cpu().numpy(), requires_grad=true)
+    x = torch.Tensor(ensureimage(x).cpu().numpy(), requires_grad=true)
     return x
