f8678d22729ae2206747629b87ed4c4912e1bf82
teletravail <adrienchanhonton@gmail.com>
2021-04-13 21:53:30 +0200

    ++

--- data_agglomeration/dependencyfreeimgaug.py
+++ data_agglomeration/dependencyfreeimgaug.py
@@ -9,54 +9,43 @@ def ensureimage(x, device="cuda"):
     )
 
 
-def pepperandsalt(x, nbpixel=30, device="cuda"):
+def pepperandsalt(k, x, nbpixel=30, device="cuda"):
     row = np.random.randint(0, x.shape[2], size=nbpixel)
     col = np.random.randint(0, x.shape[3], size=nbpixel)
     pepperorsalt = np.random.randint(0, 2, size=nbpixel)
     for j in range(3):
         for i in range(nbpixel):
-            x[:, j, row[i], col[i]] = torch.ones(1).to(device) * float(pepperorsalt[i])
-    return x
+            x[k, j, row[i], col[i]] = torch.ones(1).to(device) * float(pepperorsalt[i])
 
 
-def shift(x, level=30, device="cuda"):
+def shift(k, x, level=30, device="cuda"):
     for j in range(3):
-        x[:, j, :, :] += (torch.rand().to(device) * 2 - 1) * level / 255
-    return ensureimage(x)
+        x[k, j, :, :] += (torch.rand().to(device) * 2 - 1) * level / 255
 
 
-def blur(x, level=2, device="cuda"):
-    return torch.nn.functional.max_pool2d(
-        x, kernel_size=2 * level + 1, padding=level, stride=1
-    )
+def gaussian(k, x, level=5, device="cuda"):
+    x[k] += (torch.randn(x[k].shape) * level / 255).to(device)
 
 
-def gaussian(x, level=5, device="cuda"):
-    return ensureimage(x + torch.randn(x.shape).to(device) * level / 255)
+def uniform(k, x, level=10, device="cuda"):
+    x[k] += ((torch.rand(x[k].shape) * 2 - 1) * level / 255).to(device)
 
 
-def uniform(x, level=10, device="cuda"):
-    return ensureimage(x + (torch.rand(x.shape) * 2 - 1).to(device) * level / 255)
-
-
-def cutout(x, level=5, device="cuda"):
+def cutout(k, x, level=5, device="cuda"):
     row = random.randint(0, x.shape[2] - level - 1)
     col = random.randint(0, x.shape[3] - level - 1)
-    x[:, :, row : row + level, col : col + level] = float(random.randint(0, 1))
-    return x
+    x[k, :, row : row + level, col : col + level] = float(random.randint(0, 1))
 
 
-def dropchannel(x, device="cuda"):
-    x[:, random.randint(0, 2), :, :] = float(random.randint(0, 1))
-    return x
+def dropchannel(k, x, device="cuda"):
+    x[k, random.randint(0, 2), :, :] = float(random.randint(0, 1))
 
 
-def gray(x, device="cuda"):
-    tmp = 0.333 * (x[:, 0, :, :] + x[:, 1, :, :] + x[:, 2, :, :])
-    x[:, 0, :, :] = tmp
-    x[:, 1, :, :] = tmp
-    x[:, 2, :, :] = tmp
-    return x
+def gray(k, x, device="cuda"):
+    tmp = 0.333 * (x[k, 0, :, :] + x[k, 1, :, :] + x[k, 2, :, :])
+    x[k, 0, :, :] = tmp
+    x[k, 1, :, :] = tmp
+    x[k, 2, :, :] = tmp
 
 
 def augment(x, device="cuda"):
@@ -65,24 +54,26 @@ def augment(x, device="cuda"):
             if random.randint(0, 10) == 0:
                 # large data augment
                 if random.randint(0, 1) == 0:
-                    x[i : i + 1] = dropchannel(x[i : i + 1], device=device)
+                    dropchannel(i, x, device=device)
                 else:
-                    x[i : i + 1] = gray(x[i : i + 1], device=device)
+                    gray(i, x, device=device)
             else:
                 # small data augment
-                i = random.randint(0, 5)
-                if i == 0:
-                    x[i : i + 1] = pepperandsalt(x[i : i + 1], device=device)
-                if i == 1:
-                    x[i : i + 1] = shift(x[i : i + 1], device=device)
-                if i == 2:
-                    x[i : i + 1] = blur(x[i : i + 1], device=device)
-                if i == 3:
-                    x[i : i + 1] = gaussian(x[i : i + 1], device=device)
-                if i == 4:
-                    x[i : i + 1] = uniform(x[i : i + 1], device=device)
-                if i == 5:
-                    x[i : i + 1] = cutout(x[i : i + 1], device=device)
+                j = random.randint(0, 5)
+                if j == 0:
+                    pepperandsalt(i, x, device=device)
+                if j == 1:
+                    shift(i, x, device=device)
+                if j == 2:
+                    x[i : i + 1] = torch.nn.functional.max_pool2d(
+                        x[i : i + 1], kernel_size=2 * level + 1, padding=level, stride=1
+                    )
+                if j == 3:
+                    gaussian(i, x, device=device)
+                if j == 4:
+                    uniform(i, x, device=device)
+                if j == 5:
+                    cutout(i, x, device=device)
 
     x = torch.Tensor(x.cpu().numpy(), requires_grad=true)
     return x
