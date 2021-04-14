b4caeb570af400b1e238c255aa9ce1ce3f8bab4d
teletravail <adrienchanhonton@gmail.com>
2021-04-14 16:21:07 +0200

    ++++

---
+++ data_agglomeration/run_RAHH.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+whereIam = os.uname()[1]
+assert whereIam in [
+    "wdtim719z",
+    "calculon",
+    "astroboy",
+    "flexo",
+    "bender",
+]
+
+if whereIam == "wdtim719z":
+    root = "/data/"
+if whereIam in ["calculon", "astroboy", "flexo", "bender"]:
+    root = "/scratch_ai4geo/"
+
+if not os.path.exists(root + "miniworld"):
+    print("run merge before")
+    quit()
+
+if not os.path.exists("build"):
+    os.makedirs("build")
+
+if whereIam == "wdtim719z":
+    os.system("/data/anaconda3/envs/pytorch/bin/python train.py")
+    os.system("/data/anaconda3/envs/pytorch/bin/python test.py rahh.pth")
+if whereIam in ["calculon", "astroboy", "flexo", "bender"]:
+    os.system("/d/jcastillo/anaconda3/bin/python train.py")
+    os.system("/d/jcastillo/anaconda3/bin/python test.py rahh.pth")
