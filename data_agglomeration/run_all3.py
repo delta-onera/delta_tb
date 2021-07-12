56ac9e6cf45f670dc6f24ef183e03255d91fd461
teletravail <adrienchanhonton@gmail.com>
2021-07-12 15:21:24 +0200

    ++

--- data_agglomeration/run_all3.py
+++ data_agglomeration/run_all3.py
@@ -5,6 +5,8 @@ import random
 
 whereIam = os.uname()[1]
 assert whereIam in [
+    "super",
+    "ldtis706z",
     "wdtim719z",
     "calculon",
     "astroboy",
@@ -12,8 +14,10 @@ assert whereIam in [
     "bender",
 ]
 
-if whereIam == "wdtim719z":
+if whereIam in ["wdtim719z", "super"]:
     root = "/data/"
+if whereIam == "ldtis706z":
+    root = "/media/achanhon/bigdata/data/"
 if whereIam in ["calculon", "astroboy", "flexo", "bender"]:
     root = "/scratch_ai4geo/"
 
@@ -29,9 +33,19 @@ tmp = random.randint(0, 1000)
 myhash = str(today) + "_" + str(tmp)
 print(myhash)
 
+if whereIam == "super":
+    os.system("/data/anaconda3/bin/python train3.py build/" + myhash + ".pth")
+    os.system("/data/anaconda3/bin/python test3.py build/" + myhash + ".pth")
 if whereIam == "wdtim719z":
-    os.system("/data/anaconda3/envs/pytorch/bin/python train3.py " + myhash)
-    os.system("/data/anaconda3/envs/pytorch/bin/python test3.py " + myhash)
+    os.system(
+        "/data/anaconda3/envs/pytorch/bin/python train3.py build/" + myhash + ".pth"
+    )
+    os.system(
+        "/data/anaconda3/envs/pytorch/bin/python test3.py build/" + myhash + ".pth"
+    )
+if whereIam == "ldtis706z":
+    os.system("python3 train3.py build/" + myhash + ".pth")
+    os.system("python3 test3.py build/" + myhash + ".pth")
 if whereIam in ["calculon", "astroboy", "flexo", "bender"]:
-    os.system("/d/jcastillo/anaconda3/bin/python train3.py " + myhash)
-    os.system("/d/jcastillo/anaconda3/bin/python test3.py " + myhash)
+    os.system("/d/jcastillo/anaconda3/bin/python train3.py build/" + myhash + ".pth")
+    os.system("/d/jcastillo/anaconda3/bin/python test3.py build/" + myhash + ".pth")
