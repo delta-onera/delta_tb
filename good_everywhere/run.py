a1f78d0dfe7d59d59902bc52116d7c6d73bf7c58
teletravail <adrienchanhonton@gmail.com>
2021-10-25 09:34:41 +0200

    ==>
    optimom : on test ville par ville
    pti + deeplab : on test la nouvelle priorisation
    
    => d'abord je vais tester les modèles existant en train pour voir si pb
    généralisation

--- good_everywhere/run.py
+++ good_everywhere/run.py
@@ -20,25 +20,50 @@ os.system("rm -rf build")
 os.makedirs("build")
 
 if whereIam == "wdtim719z":
-    os.system("/data/anaconda3/envs/pytorch/bin/python -u min.py")
-    os.system("/data/anaconda3/envs/pytorch/bin/python -u test.py")
-    os.system("/data/anaconda3/envs/pytorch/bin/python -u freq.py")
-    os.system("/data/anaconda3/envs/pytorch/bin/python -u test.py")
-    os.system("/data/anaconda3/envs/pytorch/bin/python -u normal.py")
-    os.system("/data/anaconda3/envs/pytorch/bin/python -u test.py")
+    self.cities = [
+        "potsdam",
+        "christchurch",
+        "toulouse",
+        "austin",
+        "chicago",
+        "kitsap",
+        "tyrol-w",
+        "vienna",
+        "bruges",
+        "Arlington",
+        "Austin",
+        "DC",
+        "NewYork",
+        "SanFrancisco",
+        "Atlanta",
+        "NewHaven",
+        "Norfolk",
+        "Seekonk",
+    ]
+    for city in cities:
+        print(city)
+        os.system("/data/anaconda3/envs/pytorch/bin/python -u normal.py " + city)
+        os.system("/data/anaconda3/envs/pytorch/bin/python -u test.py")
+        os.system("/data/anaconda3/envs/pytorch/bin/python -u test.py")
 if whereIam == "ldtis706z":
+    print("freq")
     os.system("python3 -u freq.py")
-    os.system("python3 -u test.py")
+    os.system("python3 -u test.py debug")
+    print("normal")
     os.system("python3 -u normal.py")
-    os.system("python3 -u test.py")
+    os.system("python3 -u test.py debug")
+    print("min")
     os.system("python3 -u min.py")
-    os.system("python3 -u test.py")
+    os.system("python3 -u test.py debug")
 if whereIam in ["calculon", "astroboy", "flexo", "bender"]:
-    os.system("/d/jcastillo/anaconda3/bin/python -u normal.py")
-    os.system("/d/jcastillo/anaconda3/bin/python -u test.py")
+    print("freq")
     os.system("/d/jcastillo/anaconda3/bin/python -u freq.py")
     os.system("/d/jcastillo/anaconda3/bin/python -u test.py")
-    os.system("/d/jcastillo/anaconda3/bin/python -u min.py")
-    os.system("/d/jcastillo/anaconda3/bin/python -u test.py")
+    # print("normal")
+    # os.system("/d/jcastillo/anaconda3/bin/python -u normal.py")
+    # os.system("/d/jcastillo/anaconda3/bin/python -u test.py")
+    # print("min")
+    # os.system("/d/jcastillo/anaconda3/bin/python -u min.py")
+    # os.system("/d/jcastillo/anaconda3/bin/python -u test.py")
 
 os.system("rm -rf __pycache__")
