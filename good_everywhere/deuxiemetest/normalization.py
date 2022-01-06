9213ba51b307b9f7eaf198b9349652a3e9de892b
teletravail <adrienchanhonton@gmail.com>
2022-01-06 13:20:15 +0100

    ça marche !!!!!!

--- good_everywhere/deuxiemetest/normalization.py
+++ good_everywhere/deuxiemetest/normalization.py
@@ -28,6 +28,14 @@ def minmax(image, removeborder=True):
     return out
 
 
+def debughisto(image):
+    tot = image.shape[0] * image.shape[1]
+    s = "histo"
+    for i in range(256):
+        s += "\t" + str(int(numpy.sum(numpy.int32(image == i)) * 1000.0 / tot))
+    print(s)
+
+
 def printhisto(histo):
     s = "histo"
     for i in histo:
@@ -49,7 +57,7 @@ def computehisto(image):
     return source
 
 
-def histogrammatching(source, cible):
+def histogrammatchingold(source, cible):
     j, flag = 0, True
     matching = {}
     while len(source) != 0:
@@ -80,25 +88,50 @@ def convert(image, matching):
     return minmax(output, removeborder=False)
 
 
+def histogrammatching(image, tmpl):
+    # inspired from scikit-image/blob/main/skimage/exposure/histogram_matching.py
+    tmpl_quantiles, tmpl_val = tmpl
+
+    _, src_indices, src_counts = numpy.unique(
+        image.flatten(), return_inverse=True, return_counts=True
+    )
+
+    # ensure single value can not distord the histogram
+    cut = numpy.ones(src_counts.shape) * image.shape[0] * image.shape[1] / 20
+    src_counts = numpy.minimum(src_counts, cut)
+    src_quantiles = numpy.cumsum(src_counts)
+    src_quantiles = src_quantiles / src_quantiles[-1]
+
+    interp_a_values = numpy.interp(src_quantiles, tmpl_quantiles, tmpl_val)
+    return interp_a_values[src_indices].reshape(image.shape)
+
+
 class ManyHistogram:
     def __init__(self):
         self.cibles = numpy.zeros((5, 256))
 
-        centers = [256 // 3, 256 // 2, 256 * 2 // 3]
+        centers = [256 // 4, 256 // 2, 256 * 3 // 4]
         for c in range(3):
             for i in range(256):
-                self.cibles[c][i] = 10.0 * math.exp(-((centers[c] - i) ** 2) / 255) + 1
+                self.cibles[c][i] = 20.0 * math.exp(-((centers[c] - i) ** 2) / 255) + 1
 
         for i in range(256):
             self.cibles[3][i] = 256 // 2 - abs(i - 256 // 2) + 2
 
         self.cibles[4] = numpy.ones(256)
 
+        self.cumsum = []
+        for i in range(5):
+            vals = numpy.arange(256)
+            quantiles = numpy.cumsum(self.cibles[i])
+            quantiles = quantiles / quantiles[-1]
+            self.cumsum.append((quantiles, vals))
+
         self.cibles = numpy.float32(self.cibles)
         for i in range(5):
             self.cibles[i] = self.cibles[i] / numpy.sum(self.cibles[i])
 
-    def normalize(self, image):
+    def normalizeold(self, image):
         image = numpy.int32(image)
         out = numpy.zeros((18, image.shape[0], image.shape[1]))
         out = numpy.int16(out)
@@ -106,8 +139,23 @@ class ManyHistogram:
         source = [computehisto(image[:, :, i]) for i in range(3)]
         for i in range(5):
             for ch in range(3):
-                tmp = histogrammatching(source[ch].copy(), self.cibles[i].copy())
+                tmp = histogrammatchingold(source[ch].copy(), self.cibles[i].copy())
                 out[i * 3 + ch] = convert(image[:, :, ch], tmp)
+
+        out[15] = minmax(image[:, :, 0])
+        out[16] = minmax(image[:, :, 1])
+        out[17] = minmax(image[:, :, 2])
+
+        return out
+
+    def normalize(self, image):
+        image = numpy.int32(image)
+        out = numpy.zeros((18, image.shape[0], image.shape[1]))
+        out = numpy.int16(out)
+
+        for i in range(5):
+            for ch in range(3):
+                out[i * 3 + ch] = histogrammatching(image[:, :, ch], self.cumsum[i])
 
         out[15] = minmax(image[:, :, 0])
         out[16] = minmax(image[:, :, 1])
@@ -127,11 +175,41 @@ if __name__ == "__main__":
 
     images = normalizations.normalize(image)
 
+    debughisto(image[:,:,0])
     for i in range(6):
         debug = images[3 * i : 3 * i + 3, :, :]
-        debug = numpy.transpose(debug, axes=(1, 2, 0))
-        debug = PIL.Image.fromarray(numpy.uint8(debug))
+        debug = numpy.uint8(numpy.transpose(debug, axes=(1, 2, 0)))
+        debughisto(debug[0])
+        debug = PIL.Image.fromarray(debug)
         debug.save("build/test8_" + str(i) + ".png")
+
+    print("debug")
+    image = PIL.Image.open("/data/miniworld/bruges/train/1_x.png").convert("RGB").copy()
+    image = numpy.uint8(numpy.asarray(image))
+
+    vals, src_counts = numpy.unique(image[:, :, 0].flatten(), return_counts=True)
+    quantiles = numpy.cumsum(src_counts)
+    quantiles = quantiles / quantiles[-1]
+    R = histogrammatching(image[:, :, 0], (quantiles, vals))
+    R = numpy.uint8(R)
+    print(numpy.sum(numpy.absolute(R-image[:, :, 0])))
+    debughisto(R)
+
+    vals, src_counts = numpy.unique(image[:, :, 1].flatten(), return_counts=True)
+    quantiles = numpy.cumsum(src_counts)
+    quantiles = quantiles / quantiles[-1]
+    G = histogrammatching(image[:, :, 1], (quantiles, vals))
+
+    vals, src_counts = numpy.unique(image[:, :, 2].flatten(), return_counts=True)
+    quantiles = numpy.cumsum(src_counts)
+    quantiles = quantiles / quantiles[-1]
+    B = histogrammatching(image[:, :, 2], (quantiles, vals))
+
+    RGB = numpy.stack([R, G, B], axis=-1)
+    debug = PIL.Image.fromarray(numpy.uint8(RGB))
+    debug.save("build/debug.png")
+
+    quit()
 
     import rasterio
 
