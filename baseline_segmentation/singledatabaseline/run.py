2a866770850ab73a9a8399057d63cfc716559c7e
teletravail <adrienchanhonton@gmail.com>
2022-01-09 09:02:40 +0100

    +++

---
+++ baseline_segmentation/singledatabaseline/run.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+whereIam = os.uname()[1]
+assert whereIam in ["wdtim719z", "calculon", "astroboy", "flexo", "bender", "ldtis706z"]
+
+os.system("rm -rf __pycache__")
+os.system("rm -rf build")
+os.makedirs("build")
+
+if whereIam == "wdtim719z":
+    os.system("/data/anaconda3/envs/pytorch/bin/python -u train.py")
+    os.system("/data/anaconda3/envs/pytorch/bin/python -u test.py")
+if whereIam == "ldtis706z":
+    os.system("python3 -u train.py")
+    os.system("python3 -u test.py")
+if whereIam in ["calculon", "astroboy", "flexo", "bender"]:
+    os.system("/d/jcastillo/anaconda3/bin/python -u train.py")
+    os.system("/d/jcastillo/anaconda3/bin/python -u test.py")
+
+os.system("rm -rf __pycache__")
---
+++ baseline_segmentation/singledatabaseline/train.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import numpy
+import PIL
+from PIL import Image
+import torch
+import torch.backends.cudnn as cudnn
+
+if torch.cuda.is_available():
+    torch.cuda.empty_cache()
+    cudnn.benchmark = True
+else:
+    print("no cuda")
+    quit()
+
+whereIam = os.uname()[1]
+if whereIam == "ldtis706z":
+    sys.path.append("/home/achanhon/github/EfficientNet-PyTorch")
+    sys.path.append("/home/achanhon/github/pytorch-image-models")
+    sys.path.append("/home/achanhon/github/pretrained-models.pytorch")
+    sys.path.append("/home/achanhon/github/segmentation_models.pytorch")
+if whereIam == "wdtim719z":
+    sys.path.append("/home/optimom/github/EfficientNet-PyTorch")
+    sys.path.append("/home/optimom/github/pytorch-image-models")
+    sys.path.append("/home/optimom/github/pretrained-models.pytorch")
+    sys.path.append("/home/optimom/github/segmentation_models.pytorch")
+if whereIam in ["calculon", "astroboy", "flexo", "bender"]:
+    sys.path.append("/d/achanhon/github/EfficientNet-PyTorch")
+    sys.path.append("/d/achanhon/github/pytorch-image-models")
+    sys.path.append("/d/achanhon/github/pretrained-models.pytorch")
+    sys.path.append("/d/achanhon/github/segmentation_models.pytorch")
+
+import segmentation_models_pytorch as smp
+import cropextractor
+import dataloader
+
+print("load data")
+miniworld = dataloader.MiniWorld(flag="train")
+
+print("define model")
+net = smp.Unet(
+    encoder_name="efficientnet-b7",
+    encoder_weights="imagenet",
+    in_channels=3,
+    classes=2,
+)
+net = net.cuda()
+net.train()
+
+
+print("train")
+optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
+if whereIam in ["ldtis706z", "wdtim719z"]:
+    batchsize = 16
+else:
+    batchsize = 32
+nbbatchs = 200000
+printloss = torch.zeros(1).cuda()
+stats = torch.zeros((len(miniworld.cities), 2, 2)).cuda()
+miniworld.start()
+
+for i in range(nbbatchs):
+    x, y, batchchoise = miniworld.getbatch(batchsize)
+    x, y = x.cuda(), y.cuda()
+    z = net(x)
+
+    D = dataloader.distancetransform(y)
+    nb0, nb1 = torch.sum((y == 0).float()), torch.sum((y == 1).float())
+    weights = torch.Tensor([1, nb0 / (nb1 + 1)]).cuda()
+    criterion = torch.nn.CrossEntropyLoss(weight=weights, reduction="none")
+    CE = criterion(z, y.long())
+    CE = CE * D
+    CE = torch.mean(CE)
+
+    criteriondice = smp.losses.dice.DiceLoss(mode="multiclass")
+    dice = criteriondice(z, y.long())
+    loss = CE + 0.1 * dice
+
+    with torch.no_grad():
+        printloss += loss.clone().detach()
+    if i > nbbatchs * 0.1:
+        loss = loss * 0.5
+    if i > nbbatchs * 0.2:
+        loss = loss * 0.5
+    if i > nbbatchs * 0.5:
+        loss = loss * 0.5
+    if i > nbbatchs * 0.8:
+        loss = loss * 0.5
+
+    optimizer.zero_grad()
+    loss.backward()
+    torch.nn.utils.clip_grad_norm_(net.parameters(), 3)
+    optimizer.step()
+
+    with torch.no_grad():
+        z = (z[:, 1, :, :] > z[:, 0, :, :]).float()
+        for j in range(batchsize):
+            cm = torch.zeros(2, 2).cuda()
+            for a, b in [(0, 0), (0, 1), (1, 0), (1, 1)]:
+                cm[a][b] = torch.sum((z[j] == a).float() * (y[j] == b).float() * D[j])
+            stats[batchchoise[j]] += cm
+
+    if i % 100 == 99:
+        print(i, "/200000", printloss / 100)
+        printloss = torch.zeros(1).cuda()
+
+    if i % 1000 == 999:
+        torch.save(net, "build/model.pth")
+        cm = torch.sum(stats, dim=0)
+        perf = dataloader.perf(cm)
+        print("perf", perf)
+        if perf[0] > 92:
+            print("training stops after reaching high training accuracy")
+            os._exit(0)
+        else:
+            stats = torch.zeros((len(miniworld.cities), 2, 2)).cuda()
+
+print("training stops after reaching time limit")
+os._exit(0)
