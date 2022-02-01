e33cc8499668475c588c0fdc1784c768b6e99642
achanhon <adrien.chan_hon_tong@onera.fr>
2022-02-01 15:30:33 +0100

    +

--- good_everywhere/simpletransfert/test.py
+++ good_everywhere/simpletransfert/test.py
@@ -80,7 +80,7 @@ with torch.no_grad():
 
             if True:
                 nextI = len(os.listdir("build"))
-                debug = cropextractor.torchTOpil(globalresize(x))
+                debug = dataloader.torchTOpil(globalresize(x))
                 debug = PIL.Image.fromarray(numpy.uint8(debug))
                 debug.save("build/" + str(nextI) + "_x.png")
                 debug = (2.0 * y - 1) * D * 127 + 127
