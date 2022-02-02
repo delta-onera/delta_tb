32fee7b7fcaafc52467a04930935d403aadd35eb
teletravail <adrienchanhonton@gmail.com>
2022-02-02 19:49:20 +0100

    ++

--- good_everywhere/simpletransfert/test.py
+++ good_everywhere/simpletransfert/test.py
@@ -34,7 +34,7 @@ import segmentation_models_pytorch as smp
 import dataloader
 
 print("load data")
-miniworld = dataloader.PhysicalData(names=["2_Vegas"], flag="flat")
+miniworld = dataloader.PhysicalData(names=["AOI_2_Vegas_Train"], flag="flat")
 
 print("load model")
 with torch.no_grad():
