a6697d30f292072f0b3d88b657808a5ae1e8f8e4
teletravail <adrienchanhonton@gmail.com>
2022-02-03 10:53:58 +0100

    ++

--- good_everywhere/baseline8bits/train.py
+++ good_everywhere/baseline8bits/train.py
@@ -104,7 +104,7 @@ for i in range(nbbatchs):
                 cm[a][b] = torch.sum((z[j] == a).float() * (y[j] == b).float() * D[j])
             stats[batchchoise[j]] += cm
 
-    if i % 100 == 99:
+    if i % 100 == 9:
         print(i, "/200000", printloss / 100)
         printloss = torch.zeros(1).cuda()
 
