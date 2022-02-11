7aaa58836ce5901f9cc90a0cd8c5c4e20a6a5a2c
teletravail <adrienchanhonton@gmail.com>
2022-02-11 12:30:42 +0100

    +++

--- baseline_segmentation/torchvision/run.py
+++ baseline_segmentation/torchvision/run.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 if len(sys.argv) == 1:
     print("YOU NEED TO PROVIDE PATH TO DATA")
@@ -9,7 +10,9 @@ os.system("rm -rf __pycache__")
 os.system("rm -rf build")
 os.makedirs("build")
 
+t = time.time()
 os.system("python3 -u train.py " + sys.argv[1] + "/train/")
 os.system("python3 -u test.py " + sys.argv[1] + "/test/")
+print("total duration", time.time() - t)
 
 os.system("rm -rf __pycache__")
