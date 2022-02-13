40203af0de04e9eeb20fc81b34ed3c206620517a
teletravail <adrienchanhonton@gmail.com>
2022-02-13 09:59:54 +0100

    +++

--- good_everywhere/witherosion/test.py
+++ good_everywhere/witherosion/test.py
@@ -83,6 +83,7 @@ with torch.no_grad():
             z = largeforward(net, x.unsqueeze(0))
             z = globalresize(z)
             z = (z[0, 1, :, :] > z[0, 0, :, :]).float()
+            z = erosion(z)
 
             for a, b in [(0, 0), (0, 1), (1, 0), (1, 1)]:
                 cm[k][a][b] = torch.sum((z == a).float() * (y == b).float() * D)
