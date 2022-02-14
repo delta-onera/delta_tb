8b14398fb08463a4239487b10d92d1fd3550a4ab
achanhon <adrien.chan_hon_tong@onera.fr>
2022-02-14 11:36:06 +0100

    +++

--- good_everywhere/private_data/run.py
+++ good_everywhere/private_data/run.py
@@ -6,10 +6,9 @@ os.system("rm -rf __pycache__")
 
 os.system("mkdir build")
 
-os.system("cp ../miniworld/build/model.pth build/")
+# os.system("cp ../miniworld/build/model.pth build/")
 
-# os.system("cp ../../baseline_segmentation/baseline8bits/build/model.pth build/")
-# miniworld 443099 iter - 87% en train
+os.system("cp ../../baseline_segmentation/baseline8bits/build/model.pth build/")
 
 os.system("/d/jcastillo/anaconda3/bin/python -u test.py")
 
--- good_everywhere/witherosion/run.py
+++ good_everywhere/witherosion/run.py
@@ -6,9 +6,19 @@ os.system("rm -rf __pycache__")
 
 os.system("mkdir build")
 
-os.system("cp ../miniworld/build/model.pth build/")
+# os.system("cp ../miniworld/build/model.pth build/")
+# erosion 0
+# Biarritz tensor([78.6980, 94.6378])
+# Strasbourg tensor([71.9950, 90.6731])
+# Paris tensor([75.2996, 96.8422])
+# digitanie_toulouse tensor([72.7540, 92.2183])
+# erosion 2
+# Biarritz tensor([80.0219, 95.6642])
+# Strasbourg tensor([74.5771, 92.5974])
+# Paris tensor([75.4093, 97.2974])
+# digitanie_toulouse tensor([76.6753, 94.3347])
 
-# os.system("cp ../../baseline_segmentation/baseline8bits/build/model.pth build/")
+os.system("cp ../../baseline_segmentation/baseline8bits/build/model.pth build/")
 # miniworld 443099 iter - 87% en train
 
 os.system("/d/jcastillo/anaconda3/bin/python -u test.py")
