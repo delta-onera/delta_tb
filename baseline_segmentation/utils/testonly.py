77dc2dbd526632ed7b7ffd7e692e99bf517f86c9
teletravail <adrienchanhonton@gmail.com>
2022-02-16 09:26:43 +0100

    ++

---
+++ baseline_segmentation/utils/testonly.py
@@ -0,0 +1,42 @@
+import os
+import PIL
+from PIL import Image
+import numpy
+
+if len(sys.argv) == 1:
+    print("wrong path")
+    quit()
+TODO = sys.argv[1] + "/"
+print(TODO)
+
+
+def perf(cm):
+    accu = 100.0 * (cm[0][0] + cm[1][1]) / (torch.sum(cm) + 1)
+    iou0 = 50.0 * cm[0][0] / (cm[0][0] + cm[1][0] + cm[0][1] + 1)
+    iou1 = 50.0 * cm[1][1] / (cm[1][1] + cm[1][0] + cm[0][1] + 1)
+    return torch.Tensor((iou0 + iou1, accu))
+
+
+found = set(os.listdir(TODO))
+names = [name for name in found if "y.png" in name]
+names = [name[:-5] for name in names]
+names = [name for name in names if name + "z.png" in found]
+
+cm = numpy.zeros((2, 2))
+
+for name in names:
+    y = PIL.Image.open(TODO + name + "y.png").convert("L").copy()
+    z = PIL.Image.open(TODO + name + "z.png").convert("L").copy()
+
+    z = numpy.int16(z > 125)
+    D = numpy.absolute(y - 125) / 125
+    y = numpy.int16(y > 125)
+
+    for a, b in [(0, 0), (0, 1), (1, 0), (1, 1)]:
+        cm[a][b] += numpy.sum(numpy.int16(z == a) * numpy.int16(y == b) * D)
+
+accu = 100.0 * (cm[0][0] + cm[1][1]) / (torch.sum(cm) + 1)
+iou0 = 50.0 * cm[0][0] / (cm[0][0] + cm[1][0] + cm[0][1] + 1)
+iou1 = 50.0 * cm[1][1] / (cm[1][1] + cm[1][0] + cm[0][1] + 1)
+print(iou0 + iou1)
+print(accu, 2 * iou0, 2 * iou1, cm)
