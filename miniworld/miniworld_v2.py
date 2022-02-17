a684ea128eadcdfd17f64ce5972e4602bc8c3d2e
achanhon <adrien.chan_hon_tong@onera.fr>
2022-02-17 10:54:44 +0100

    ++

--- miniworld/miniworld_v2.py
+++ miniworld/miniworld_v2.py
@@ -6,19 +6,19 @@ import json
 import csv
 
 root = "/scratchf/"
-rootminiworld = "/scratchf/miniworld_1M/"
+rootminiworld = "/scratchf/miniworld_wtf/"
 
 if os.path.exists(rootminiworld):
     os.system("rm -rf " + rootminiworld)
     os.makedirs(rootminiworld)
 
-TARGET_RESOLUTION = 100.0
+TARGET_RESOLUTION = 50.0
 TODO = {}
-TODO["bradbery"] = root + "DATASETS/BRADBURY_BUILDING_HEIGHT/"
-TODO["dfc"] = root + "DFC2015/"
-TODO["isprs"] = root + "DATASETS/ISPRS_POTSDAM/"
-TODO["inria"] = root + "DATASETS/INRIA/AerialImageDataset/train/"
-TODO["landcover"] = root + "landcover.ai.v1/"
+# TODO["bradbery"] = root + "DATASETS/BRADBURY_BUILDING_HEIGHT/"
+# TODO["dfc"] = root + "DFC2015/"
+# TODO["isprs"] = root + "DATASETS/ISPRS_POTSDAM/"
+# TODO["inria"] = root + "DATASETS/INRIA/AerialImageDataset/train/"
+# TODO["landcover"] = root + "landcover.ai.v1/"
 TODO["airs"] = root + "DATASETS/AIRS/trainval/"
 TODO["spacenet1"] = root + "DATASETS/SPACENET1/train/"
 
