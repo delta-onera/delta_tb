1caf1b1f7e6ae0228d17a8ad74bde734fb3ca4eb
teletravail <adrienchanhonton@gmail.com>
2022-02-18 21:06:38 +0100

    ++

--- good_everywhere/learning_small/train.py
+++ good_everywhere/learning_small/train.py
@@ -52,8 +52,6 @@ def diceloss(y, z, D):
     z0, z1 = z[:, 0, :, :], z[:, 1, :, :]
     y0, y1 = (y == 0).float(), (y == 1).float()
 
-    print(y.shape, z.shape, y0.shape, z0.shape)
-
     inter0, inter1 = (y0 * z0 * D).sum(), (y1 * z1 * D).sum()
     union0, union1 = ((y0 + z1 * y0) * D).sum(), ((y1 + z0 * y1) * D).sum()
     iou0, iou1 = (inter0 + eps) / (union0 + eps), (inter1 + eps) / (union1 + eps)
@@ -70,9 +68,7 @@ for i in range(nbbatchs):
     D = dataloader.distancetransform(y.float())
     CE = criterion(z, y)
     CE = torch.mean(CE * D)
-
-    tmp = torch.stack([D, D], dim=1)
-    dice = diceloss(y, z, tmp)
+    dice = diceloss(y, z, D)
     loss = CE + dice
 
     with torch.no_grad():
