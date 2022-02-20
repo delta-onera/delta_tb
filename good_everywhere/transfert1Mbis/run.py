d6c2e61a5c2694b36eda1bc734482d2d020e075c
teletravail <adrienchanhonton@gmail.com>
2022-02-20 13:47:10 +0100

    ++

--- good_everywhere/transfert1Mbis/run.py
+++ good_everywhere/transfert1Mbis/run.py
@@ -5,10 +5,10 @@ os.system("rm -rf build")
 os.system("rm -rf __pycache__")
 os.system("mkdir build")
 
-os.system("cp ../segmentation1M/build/model.pth build/")
+os.system("cp ../learning_1M/build/model.pth build/")
 
 if len(sys.argv) == 1:
-    os.system("/d/jcastillo/anaconda3/bin/python -u test.py 0")
+    os.system("/d/jcastillo/anaconda3/bin/python -u test.py 2")
 else:
     os.system("/d/jcastillo/anaconda3/bin/python -u test.py " + sys.argv[1])
 
