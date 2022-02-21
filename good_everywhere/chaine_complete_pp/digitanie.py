0fa84da45afa330044e3a53a6a0a086810992e74
teletravail <adrienchanhonton@gmail.com>
2022-02-21 14:32:07 +0100

    +++

---
+++ good_everywhere/chaine_complete_pp/digitanie.py
@@ -0,0 +1,158 @@
+import os
+import sys
+import numpy
+import PIL
+from PIL import Image
+import rasterio
+import torch
+import random
+
+
+def pilTOtorch(x):
+    return torch.Tensor(numpy.transpose(x, axes=(2, 0, 1)))
+
+
+def torchTOpil(x):
+    return numpy.transpose(x.cpu().numpy(), axes=(1, 2, 0))
+
+
+def distancetransform(y, size=4):
+    yy = 2.0 * y.unsqueeze(0) - 1
+    yyy = torch.nn.functional.avg_pool2d(
+        yy, kernel_size=2 * size + 1, stride=1, padding=size
+    )
+    D = 1.0 - 0.5 * (yy - yyy).abs()
+    return D[0]
+
+
+def perf(cm):
+    if len(cm.shape) == 2:
+        accu = 100.0 * (cm[0][0] + cm[1][1]) / (torch.sum(cm) + 1)
+        iou0 = 50.0 * cm[0][0] / (cm[0][0] + cm[1][0] + cm[0][1] + 1)
+        iou1 = 50.0 * cm[1][1] / (cm[1][1] + cm[1][0] + cm[0][1] + 1)
+        return torch.Tensor((iou0 + iou1, accu, iou0 * 2, iou1 * 2))
+    else:
+        out = torch.zeros(cm.shape[0], 4)
+        for k in range(cm.shape[0]):
+            out[k] = perf(cm[k])
+        return out
+
+
+def resize(image, label):
+    size = (image.size[0] // 2, image.size[1] // 2)
+    image = image.resize(size, PIL.Image.BILINEAR)
+    label = label.resize(size, PIL.Image.NEAREST)
+    return image, label
+
+
+def resizenumpy(image, label):
+    image = PIL.Image.fromarray(numpy.uint8(image))
+    label = PIL.Image.fromarray(numpy.uint8(label))
+    image, label = resize(image=image, label=label)
+    return numpy.asarray(image), numpy.asarray(label)
+
+
+########################################################################
+######################### PRIVATE CNES DATASET #########################
+
+
+def digitanie_name():
+    return ["Biarritz", "Strasbourg", "Paris"]
+
+
+class DIGITANIE:
+    def __init__(self, name, path):
+        print("DIGITANIE", name)
+        self.path = path
+        self.name = name
+
+        tmp = path + name + "/" + name.lower() + "_tuile_"
+        self.NB = 0
+        while os.path.exists(tmp + str(self.NB + 1) + "_c.tif"):
+            self.NB += 1
+
+        if self.NB == 0:
+            print("wrong path", tmp)
+            quit()
+
+    def getImageAndLabel(self, i):
+        assert i < self.NB
+
+        tmp = self.path + self.name + "/" + self.name.lower() + "_tuile_"
+        xpath = tmp + str(i + 1) + "_img_c.tif"
+        ypath = tmp + str(i + 1) + "_c.tif"
+
+        with rasterio.open(xpath) as src:
+            r = numpy.int16(src.read(1) * 255)
+            g = numpy.int16(src.read(2) * 255)
+            b = numpy.int16(src.read(3) * 255)
+            x = numpy.stack([r, g, b], axis=-1)
+
+        y = PIL.Image.open(ypath).convert("L").copy()
+        y = numpy.asarray(y)
+        y = numpy.uint8((y[:, :] == 4))
+
+        return resizenumpy(x, y)
+
+
+class DIGITANIE_TOULOUSE:
+    def __init__(self, path):
+        print("DIGITANIE_TOULOUSE")
+        self.path = path
+
+        self.files = [
+            ("tlse_arenes_c.tif", "tlse_arenes_img_c.tif"),
+            ("tlse_bagatelle_c.tif", "tlse_bagatelle_img_c.tif"),
+            ("tlse_cepiere_c.tif", "tlse_cepiere_img_c.tif"),
+            ("tlse_empalot_c.tif", "tlse_empalot_img_c.tif"),
+            ("tlse_mirail_c.tif", "tlse_mirail_img_c.tif"),
+            ("tlse_montaudran_c.tif", "tlse_montaudran_img_c.tif"),
+            ("tlse_zenith_c.tif", "tlse_zenith_img_c.tif"),
+        ]
+        self.NB = len(self.files)
+
+    def getImageAndLabel(self, i):
+        assert i < self.NB
+
+        with rasterio.open(self.path + self.files[i][1]) as src:
+            r = numpy.int16(src.read(1) * 255)
+            g = numpy.int16(src.read(2) * 255)
+            b = numpy.int16(src.read(3) * 255)
+            x = numpy.stack([r, g, b], axis=-1)
+
+        y = PIL.Image.open(self.path + self.files[i][0]).convert("L").copy()
+        y = numpy.asarray(y)
+        y = numpy.uint8((y[:, :] == 4))
+
+        return resizenumpy(x, y)
+
+
+######################### PRIVATE CNES DATASET #########################
+########################################################################
+
+
+class DigitanieALL:
+    def __init__(self, names=None, path="/scratchf/PRIVATE/DIGITANIE/"):
+        if names is not None:
+            self.cities = names
+        else:
+            self.cities = digitanie_name() + ["digitanie_toulouse"]
+
+        self.data = {}
+        self.NB = {}
+        self.normalization = {}
+        for name in self.cities:
+            if name in digitanie_name():
+                self.data[name] = DIGITANIE(name, path)
+            if name == "digitanie_toulouse":
+                self.data[name] = DIGITANIE_TOULOUSE(path + "Toulouse/")
+
+            self.NB[name] = self.data[name].NB
+
+    def getImageAndLabel(self, city, i, torchformat=False):
+        x, y = self.data[city].getImageAndLabel(i)
+
+        if torchformat:
+            return pilTOtorch(x), torch.Tensor(y)
+        else:
+            return x, y
---
+++ good_everywhere/chaine_complete_pp/train.py
@@ -0,0 +1,117 @@
+import os
+import sys
+import numpy
+import PIL
+from PIL import Image
+import torch
+import torch.backends.cudnn as cudnn
+
+if torch.cuda.is_available():
+    torch.cuda.empty_cache()
+    cudnn.benchmark = True
+else:
+    print("no cuda")
+    quit()
+
+sys.path.append("/d/achanhon/github/EfficientNet-PyTorch")
+sys.path.append("/d/achanhon/github/pytorch-image-models")
+sys.path.append("/d/achanhon/github/pretrained-models.pytorch")
+sys.path.append("/d/achanhon/github/segmentation_models.pytorch")
+
+import segmentation_models_pytorch as smp
+import miniworld
+
+print("load data")
+miniworlddataset = miniworld.MiniWorld("/train/")
+
+print("define model")
+net = smp.UnetPlusPlus(
+    encoder_name="efficientnet-b7",
+    encoder_weights="imagenet",
+    in_channels=3,
+    classes=2,
+)
+net = net.cuda()
+net.train()
+
+
+print("train")
+weights = torch.Tensor([1, 1]).cuda()
+criterion = torch.nn.CrossEntropyLoss(weight=weights, reduction="none")
+optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
+printloss = torch.zeros(1).cuda()
+stats = torch.zeros((len(miniworlddataset.cities), 2, 2)).cuda()
+batchsize = 32
+nbbatchs = 200000
+miniworlddataset.start()
+
+
+def diceloss(y, z, D):
+    eps = 0.00001
+    z = z.log_softmax(dim=1).exp()
+    z0, z1 = z[:, 0, :, :], z[:, 1, :, :]
+    y0, y1 = (y == 0).float(), (y == 1).float()
+
+    inter0, inter1 = (y0 * z0 * D).sum(), (y1 * z1 * D).sum()
+    union0, union1 = ((y0 + z1 * y0) * D).sum(), ((y1 + z0 * y1) * D).sum()
+    iou0, iou1 = (inter0 + eps) / (union0 + eps), (inter1 + eps) / (union1 + eps)
+    iou = 0.5 * (iou0 + iou1)
+
+    return 1 - iou
+
+
+for i in range(nbbatchs):
+    x, y, batchchoise, _ = miniworlddataset.getBatch(batchsize)
+    x, y, batchchoise = x.cuda(), y.cuda(), batchchoise.cuda()
+    z = net(x)
+
+    D = miniworld.distancetransform(y.float())
+    CE = criterion(z, y)
+    CE = torch.mean(CE * D)
+    dice = diceloss(y, z, D)
+    loss = CE + dice
+
+    with torch.no_grad():
+        printloss += loss.clone().detach()
+        z = (z[:, 1, :, :] > z[:, 0, :, :]).clone().detach().float()
+        for j in range(batchsize):
+            cm = torch.zeros(2, 2).cuda()
+            for a, b in [(0, 0), (0, 1), (1, 0), (1, 1)]:
+                cm[a][b] = torch.sum((z[j] == a).float() * (y[j] == b).float() * D[j])
+            stats[batchchoise[j]] += cm
+
+        if i < 10:
+            print(i, "/", nbbatchs, printloss)
+        if i < 1000 and i % 100 == 99:
+            print(i, "/", nbbatchs, printloss / 100)
+            printloss = torch.zeros(1).cuda()
+        if i >= 1000 and i % 300 == 299:
+            print(i, "/", nbbatchs, printloss / 300)
+            printloss = torch.zeros(1).cuda()
+
+        if i % 1000 == 999:
+            torch.save(net, "build/model.pth")
+            perf = miniworld.perf(torch.sum(stats, dim=0))
+            print(i, "perf", perf)
+            if perf[0] > 92:
+                print("training stops after reaching high training accuracy")
+                os._exit(0)
+            else:
+                stats = torch.zeros((len(miniworlddataset.cities), 2, 2)).cuda()
+
+    if i > nbbatchs * 0.1:
+        loss = loss * 0.5
+    if i > nbbatchs * 0.2:
+        loss = loss * 0.5
+    if i > nbbatchs * 0.5:
+        loss = loss * 0.5
+    if i > nbbatchs * 0.8:
+        loss = loss * 0.5
+
+    optimizer.zero_grad()
+    loss.backward()
+    torch.nn.utils.clip_grad_norm_(net.parameters(), 3)
+    optimizer.step()
+
+print("training stops after reaching time limit")
+os._exit(0)
