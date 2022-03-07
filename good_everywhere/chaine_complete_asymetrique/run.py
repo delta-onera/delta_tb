6754e080a818f1edcf7b23c9875210dfc39f2821
teletravail <adrienchanhonton@gmail.com>
2022-03-07 09:01:45 +0100

    +

--- good_everywhere/chaine_complete_asymetrique/run.py
+++ good_everywhere/chaine_complete_asymetrique/run.py
@@ -7,6 +7,6 @@ os.makedirs("build")
 
 os.system("/d/jcastillo/anaconda3/bin/python -u train.py")
 os.system("/d/jcastillo/anaconda3/bin/python -u val.py")
-os.system("/d/jcastillo/anaconda3/bin/python -u test.py 2")
+os.system("/d/jcastillo/anaconda3/bin/python -u test.py 1")
 
 os.system("rm -rf __pycache__")
