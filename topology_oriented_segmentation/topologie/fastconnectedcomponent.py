55064c076157bce03588a8587d80300d76977236
teletravail <adrienchanhonton@gmail.com>
2022-04-22 14:07:50 +0200

    rahhh ça marche pas

--- topology_oriented_segmentation/topologie/fastconnectedcomponent.py
+++ topology_oriented_segmentation/topologie/fastconnectedcomponent.py
@@ -19,49 +19,22 @@ def isborder(y):
     return border.float()
 
 
-def minpoolH(y):
-    yy = 1 - y
-    yyy = torch.nn.functional.max_pool2d(yy, kernel_size=(3,1), stride=(1,1), padding=(1,0))
-    return 1 - yyy
-def minpoolW(y):
-    yy = 1 - y
-    yyy = torch.nn.functional.max_pool2d(yy, kernel_size=(1,3), stride=(1,1), padding=(0,1))
-    return 1 - yyy
-
-def isoleH(y):
-    yy = maxpool(minpoolH(y))
-    return (y==1).float()*(yy==0).float()
-    
-def isoleW(y):
-    yy = maxpool(minpoolW(y))
-    return (y==1).float()*(yy==0).float()
-
-
-def connected_component_seed(y):
-    with torch.no_grad():
-        erode = y.clone()
-        for i in range(15):
-            erode = (minpoolH(erode) + isoleH(erode) >= 1).float()
-            erode = (minpoolW(erode) + isoleW(erode) >= 1).float()
-        return erode
-
-
 def extract_critical_background(y):
-    CCseeds = connected_component_seed(y)
-
     with torch.no_grad():
-        tmp = torch.range(y.shape[0] * y.shape[1] * y.shape[2])
+        S = y.shape[0] * y.shape[1] * y.shape[2]
+        tmp = torch.range(1, S)
         tmp = tmp.view(y.shape)
 
-        ConnecComp1 = CCseeds * tmp
-        ConnecComp2 = CCseeds * (y.shape[0] * y.shape[1] * y.shape[2] - tmp)
-        for i in range(15):
-            ConnecComp1 = maxpool(ConnecComp1) * (y == 1).float()
-            ConnecComp2 = maxpool(ConnecComp2) * (y == 1).float()
+        ConnecComp = y * tmp
+        for i in range(20):
+            ConnecComp = maxpool(ConnecComp) * (y == 1).float()
 
+        ConnecComp1, ConnecComp2 = ConnecComp.clone(), S - ConnecComp.clone()
         for i in range(3):
             ConnecComp1 = maxpool(ConnecComp1)
             ConnecComp2 = maxpool(ConnecComp2)
+        ConnecComp2 = S - ConnecComp2
+
         return (y == 0).float() * (ConnecComp1 != ConnecComp2).float()
 
 
@@ -86,9 +59,6 @@ if __name__ == "__main__":
     border = y * (1 - border) + 0.5 * border
 
     torchvision.utils.save_image(border, "build/border.png")
-
-    ccseed = connected_component_seed(y)
-    torchvision.utils.save_image(ccseed, "build/seed.png")
 
     criticalback = extract_critical_background(y)
     torchvision.utils.save_image(criticalback, "build/critical.png")
