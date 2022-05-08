6b56361118d0203a183e0c341d5e58640fa80097
teletravail <adrienchanhonton@gmail.com>
2022-05-08 14:42:31 +0200

    +++

--- softness/all/run.py
+++ softness/all/run.py
@@ -13,6 +13,6 @@ for i in ["0", "1", "2"]:
     os.system("/d/jcastillo/anaconda3/bin/python -u val.py " + val + " " + i)
 
     test = "TEST_" + train + "size_" + i + ".csv"
-    os.system("/d/jcastillo/anaconda3/bin/python -u test.py")
+    os.system("/d/jcastillo/anaconda3/bin/python -u test.py" + test + " " + i)
 
 os.system("rm -rf __pycache__")
