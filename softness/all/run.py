733efc0a02d75943d8b9198267556c80e061ba80
teletravail <adrienchanhonton@gmail.com>
2022-05-08 11:17:01 +0200

    ++

--- softness/all/run.py
+++ softness/all/run.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-os.system("rm -rf __pycache__")
-os.system("rm -rf build")
-os.makedirs("build")
+#os.system("rm -rf __pycache__")
+#os.system("rm -rf build")
+#os.makedirs("build")
 
 # os.system("/d/jcastillo/anaconda3/bin/python -u train.py")
 
