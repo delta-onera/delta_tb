6ddb7830be1a17ac73bcd887333ffc56e27467ca
achanhon <adrien.chan_hon_tong@onera.fr>
2022-05-16 10:40:28 +0200

    deplacement

---
+++ softness/OLD/all/run.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+# os.system("rm -rf __pycache__")
+# os.system("rm -rf build")
+# os.makedirs("build")
+
+# os.system("/d/jcastillo/anaconda3/bin/python -u train.py")
+
+train = "base_size_1_stop_95_"
+for i in ["0", "1", "2"]:
+    val = "VAL_" + train + "size_" + i + ".csv"
+    os.system("/d/jcastillo/anaconda3/bin/python -u val.py " + val + " " + i)
+
+    test = "TEST_" + train + "size_" + i + ".csv"
+    os.system("/d/jcastillo/anaconda3/bin/python -u test.py " + test + " " + i)
+
+os.system("rm -rf __pycache__")
---
+++ softness/OLD/noisyAIRSold/run.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+os.system("rm -rf __pycache__")
+os.system("rm -rf build")
+os.makedirs("build")
+
+for noiselevel in ["0", "1", "2"]:
+    os.system("/d/jcastillo/anaconda3/bin/python -u noisyairs.py " + noiselevel)
+
+    configs = [
+        ("0", "border"),  # more loss on border
+        ("1", "border"),  # remove 1 border px + more loss on border
+        ("0", "noborder"),  # standard
+        ("1", "noborder"),  # remove border px
+    ]
+    for size, flag in configs:
+        trainarg = "train.py 98 " + " " + size + " " + flag
+        os.system("/d/jcastillo/anaconda3/bin/python -u " + trainarg)
+
+        if flag == "noborder":
+            trainname = "norm_level_" + noiselevel + "_size_" + size + "_"
+        else:
+            trainname = "bord_level_" + noiselevel + "_size_" + size + "_"
+        for i in ["0", "1", "2"]:
+            val = trainname + i + ".csv"
+            os.system("/d/jcastillo/anaconda3/bin/python -u val.py " + val + " " + i)
+
+    os.system("rm -rf __pycache__")
