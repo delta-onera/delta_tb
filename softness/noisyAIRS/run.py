3dc773dd02c9452c077c8ed1a3d021d885f073dc
teletravail <adrienchanhonton@gmail.com>
2022-05-19 08:30:07 +0200

    ++

--- softness/noisyAIRS/run.py
+++ softness/noisyAIRS/run.py
@@ -6,15 +6,16 @@ os.system("rm -rf build")
 os.makedirs("build")
 
 python = "/d/jcastillo/anaconda3/bin/python -u "
-for noise in ["nonoise", "hallucination", "pm1image", "pm1translation"]:
+for noise in ["nonoise", "pm1image", "hallucination", "pm1translation"]:
     for resolution in ["50cm", "1m"]:
-        name = "AIRS_" + noise + "_" + resolution + "_"
+        print("==================", noise, resolution, "===================")
+        dataname = "AIRS_" + noise + "_" + resolution + "_"
         os.system(python + "noisyairs.py " + noise + " " + resolution)
 
         for method in ["base", "base+bord", "base-bord"]:
             os.system(python + "train.py " + method)
 
-            name = name + method + "_"
+            name = dataname + method + "_"
             os.system(python + "val.py " + name)
 
     os.system("rm -rf __pycache__")
