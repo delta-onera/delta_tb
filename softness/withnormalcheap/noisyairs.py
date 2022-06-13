6e02c1350d1d2d7574dfef406ea7a01e58e59ec1
teletravail <adrienchanhonton@gmail.com>
2022-06-13 15:45:09 +0200

    ++

--- softness/withnormalcheap/noisyairs.py
+++ softness/withnormalcheap/noisyairs.py
@@ -22,7 +22,8 @@ class Sobel(torch.nn.Module):
         self.filter.weight = torch.nn.Parameter(G, requires_grad=True)
 
     def forward(self, yz):
-        self.filter.grad.zero_()
+        if self.filter.weight.grad is not None:
+            self.filter.weight.grad.zero_()
         x = self.filter(yz)
         norm = torch.sqrt(x[0] * x[0] + x[1] * x[1] + 0.001)
         x[0] = x[0] / norm
