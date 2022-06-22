54207ade053df1217e50c787c179af8fa191eed5
teletravail <adrienchanhonton@gmail.com>
2022-06-22 14:14:20 +0200

    +++

---
+++ softness/noisyAIRS/RESULTATS/merge.py
@@ -0,0 +1,27 @@
+import numpy
+
+RESOLUTION = ["30", "50", "70", "100"]
+BRUIT = ["pm1image", "nonoise", "pm1translation", "hallucination"]
+METHODE = ["base", "base-bord", "base+bord"]
+MARGE = ["0", "1", "2", "bordonly"]
+
+
+def readresults(resolution, bruit, methode, marge):
+    assert resolution in RESOLUTION
+    assert bruit in BRUIT
+    assert methode in METHODE
+    assert marge in MARGE
+
+    path = "AIRS_" + bruit + "_" + resolution + "_" + methode + "_" + marge + ".csv"
+    tmp = numpy.loadtxt(path)
+    return tmp[0] / 10
+
+
+print("check file exists")
+tmp = 0
+for bruit in BRUIT:
+    for marge in MARGE:
+        for methode in METHODE:
+            for resolution in RESOLUTION:
+                tmp += readresults(resolution, bruit, methode, marge)
+print("done")
