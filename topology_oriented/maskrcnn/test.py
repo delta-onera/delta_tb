cf91465a8b8d3c88a52373d0da357af610b5babb
achanhon <adrienchanhonton@gmail.com>
2022-10-17 19:12:15 +0200

    ++

--- topology_oriented/maskrcnn/test.py
+++ topology_oriented/maskrcnn/test.py
@@ -17,9 +17,9 @@ print("test")
 
 
 def largeforward(net, image, tilesize=512, stride=256):
-    pred = torch.zeros(2, image.shape[2], image.shape[3]).cuda()
-    for row in range(0, image.shape[2] - tilesize + 1, stride):
-        for col in range(0, image.shape[3] - tilesize + 1, stride):
+    pred = torch.zeros(2, image.shape[1], image.shape[2]).cuda()
+    for row in range(0, image.shape[1] - tilesize + 1, stride):
+        for col in range(0, image.shape[2] - tilesize + 1, stride):
             tmp = net(x=image[:, row : row + tilesize, col : col + tilesize])
             pred[:, row : row + tilesize, col : col + tilesize] += tmp[0]
     return pred
