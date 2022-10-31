c4c26cc8c15ccc7d4603ff239fb38c4c7837c286
achanhon <adrien.chan_hon_tong@onera.fr>
2022-10-31 13:49:22 +0100

    c'est quoi ce problème maintenant

--- topology_oriented/maskrcnn/train.py
+++ topology_oriented/maskrcnn/train.py
@@ -6,7 +6,7 @@ import miniworld
 assert torch.cuda.is_available()
 
 print("load data")
-root = "/scratchf/miniworld/christchurch/train/"
+root = "/scratchf/miniworld/potsdam/train/"
 dataset = miniworld.CropExtractor(root, tile=256)
 
 print("define model")
