9dc0e207c4b39b2907dd9234feef3c61c0bbcc4e
achanhon <adrienchanhonton@gmail.com>
2023-01-12 16:58:49 +0100

    ++

--- topology_oriented/watershed-raahh-digitanie/digitanieV2.py
+++ topology_oriented/watershed-raahh-digitanie/digitanieV2.py
@@ -342,13 +342,13 @@ class GlobalLocal(torch.nn.Module):
 
     def forwardlocal(self, x):
         z = torch.nn.functional.leaky_relu(self.local1(x))
-        z = torch.nn.cat([z, x], dim=1)
+        z = torch.cat([z, x], dim=1)
         z = torch.nn.functional.leaky_relu(self.local2(x))
-        z = torch.nn.cat([z, x], dim=1)
+        z = torch.cat([z, x], dim=1)
         z = torch.nn.functional.leaky_relu(self.local3(x))
-        z = torch.nn.cat([z, x], dim=1)
+        z = torch.cat([z, x], dim=1)
         z = torch.nn.functional.leaky_relu(self.local4(x))
-        z = torch.nn.cat([z, x], dim=1)
+        z = torch.cat([z, x], dim=1)
         return self.local5(x)
 
     def forward(self, x, firsttrainstep=False):
