0bdefa0c8a9a1c6465af4ffe7f6a1993ecab41b5
achanhon <adrienchanhonton@gmail.com>
2023-01-17 14:27:20 +0100

    il commence à plus y avoir de bugs

--- FLAIR_CHALLENGE/baseline_deeplab/train.py
+++ FLAIR_CHALLENGE/baseline_deeplab/train.py
@@ -48,7 +48,7 @@ def diceloss(y, z):
     return alldice.mean()
 
 
-optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
+optimizer = torch.optim.Adam(net.parameters(), lr=0.00005)
 printloss = torch.zeros(1).cuda()
 stats = torch.zeros((13, 13)).cuda()
 batchsize = 8
