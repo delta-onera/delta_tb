f41aea21c5bf0ec916cc6dfa52c2d38ed1f3a135
achanhon <adrienchanhonton@gmail.com>
2023-01-18 21:15:35 +0100

    ++

--- FLAIR_CHALLENGE/FORWARD/OLD/debug_forward.py
+++ FLAIR_CHALLENGE/FORWARD/OLD/debug_forward.py
@@ -16,13 +16,22 @@ prediction = {}
 prednames = os.listdir("../build")
 prednames = set([name for name in prednames if ".tif" in name])
 
+
+def confusion(y, z):
+    cm = torch.zeros(13, 13)
+    for a in range(13):
+        for b in range(13):
+            cm[a][b] = ((z == a).float() * (y == b).float()).sum()
+    return cm
+
+
 with torch.no_grad():
-    cm = torch.zeros((13, 13)).cuda()
+    cm = torch.zeros((13, 13))
     subdists = dataset.data.keys()
     for subdist in subdists:
         paths = dataset.data[subdist].paths
         for x, y, _ in paths:
-            tmp = torch.rand(1) * 1000
+            tmp = torch.rand(1) * 100
             if int(tmp) == 0:
                 print(cm.flatten().sum() / 512 / 512, cm[:5, :5])
 
@@ -35,11 +44,11 @@ with torch.no_grad():
             y = PIL.Image.open(y).convert("L").copy()
             y = numpy.asarray(y)
             y = numpy.clip(numpy.nan_to_num(y), 0, 12)
-            y = torch.Tensor(y).cuda()
+            y = torch.Tensor(y)
 
             z = PIL.Image.open("../build/" + name).convert("L").copy()
             z = numpy.asarray(z)
-            z = torch.Tensor(z).cuda()
+            z = torch.Tensor(z)
 
             cm += dataloader.confusion(y, z)
 
