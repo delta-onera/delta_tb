d4812e0e9f70be4086f76dd7eb4202593e074bca
achanhon <adrienchanhonton@gmail.com>
2023-01-18 11:08:49 +0100

    n'importe quoi

--- FLAIR_CHALLENGE/FORWARD/run512.py
+++ FLAIR_CHALLENGE/FORWARD/run512.py
@@ -2,10 +2,10 @@ import os
 import sys
 
 os.system("rm -rf __pycache__")
-os.system("move build/model.pth model.pth")
+os.system("mv build/model.pth model.pth")
 os.system("rm -rf build")
 os.makedirs("build")
-os.system("move model.pth build")
+os.system("mv model.pth build")
 
 os.system("/d/achanhon/miniconda3/bin/python -u test512.py")
 
