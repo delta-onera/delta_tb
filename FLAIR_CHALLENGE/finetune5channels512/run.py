8a395c30995afa323cf85e6b20ab9d09032bc742
achanhon <adrien.chan_hon_tong@onera.fr>
2023-01-22 20:52:46 +0100

    ++

--- FLAIR_CHALLENGE/finetune5channels512/run.py
+++ FLAIR_CHALLENGE/finetune5channels512/run.py
@@ -2,8 +2,10 @@ import os
 import sys
 
 os.system("rm -rf __pycache__")
+os.system("mv build/model3ch.pth model3ch.pth")
 os.system("rm -rf build")
 os.makedirs("build")
+os.system("mv model3ch.pth build")
 
 os.system("/d/achanhon/miniconda3/bin/python -u train.py")
 os.system("/d/achanhon/miniconda3/bin/python -u test.py")
