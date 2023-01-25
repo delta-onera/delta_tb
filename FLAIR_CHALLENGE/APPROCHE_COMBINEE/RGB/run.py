dda35136494db4beeef4b085af4779686e0f05b8
achanhon <adrien.chan_hon_tong@onera.fr>
2023-01-25 21:20:12 +0100

    ++

--- FLAIR_CHALLENGE/APPROCHE_COMBINEE/RGB/run.py
+++ FLAIR_CHALLENGE/APPROCHE_COMBINEE/RGB/run.py
@@ -4,8 +4,8 @@ import sys
 os.system("rm -rf build")
 os.system("mkdir build")
 os.system("cp -f ../dataloader.py build")
-os.system("cp -f ../test.py test")
-os.system("cp -f ../train.py train")
+os.system("cp -f ../test.py build")
+os.system("cp -f train.py build")
 os.system("cd build")
 
 os.system("/d/achanhon/miniconda3/bin/python -u train.py")
