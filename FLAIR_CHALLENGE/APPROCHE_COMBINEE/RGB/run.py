40f24d0fd565ef7f071fc688f8db21a0cee74384
Adrien Chan Hon Tong <you@example.com>
2023-01-25 21:56:06 +0100

    ++

--- FLAIR_CHALLENGE/APPROCHE_COMBINEE/RGB/run.py
+++ FLAIR_CHALLENGE/APPROCHE_COMBINEE/RGB/run.py
@@ -5,4 +5,4 @@ os.system("rm -rf build")
 os.system("mkdir build")
 
 os.system("/d/achanhon/miniconda3/bin/python -u train.py")
-os.system("/d/achanhon/miniconda3/bin/python -u ../test.py RGB/build")
+os.system("/d/achanhon/miniconda3/bin/python -u test.py")
