ad33cb8bb0dca2c62fb5fd5f97a90362c22370f0
achanhon <adrien.chan_hon_tong@onera.fr>
2023-02-01 17:38:26 +0100

    ++

--- FLAIR_CHALLENGE/GRANDE_IMAGE/checksplit/main.py
+++ FLAIR_CHALLENGE/GRANDE_IMAGE/checksplit/main.py
@@ -2,12 +2,41 @@ import torch
 import torchvision
 import numpy
 import rasterio
-
 import os
 
 
-def boxesTOcoords(path):
+def rankfromlist(l):
+    l = list(set(l))
+    out = {}
+    for i in range(len(l)):
+        out[l[i]] = i * 512
+    return out
+
+
+def boxesTOcoords(path, allboxes):
     boxes = numpy.load(path, allow_pickle=True)
+
+    cols = [boxes[i][1].left for i in range(boxes.shape[0])]
+    rows = [boxes[i][1].top for i in range(boxes.shape[0])]
+    cols, rows = rankfromlist(cols), rankfromlist(rows)
+
+    for i in range(boxes.shape[0]):
+        image, left, top = boxes[i][0], boxes[i][1].left, boxes[i][1].top
+        allboxes[image] = path, rows[top], cols[left]
+
+
+allboxes = {}
+domaines = os.listdir("/scratchf/flair_merged/test/")
+for domaine in domaines:
+    paths = os.listdir("/scratchf/flair_merged/test/" + domain)
+    paths = [path for paths in paths if ".npy" in path]
+    for path in paths:
+        tmp = "/scratchf/flair_merged/test/" + domain + "/" + path
+        boxesTOcoords(tmp, allboxes)
+
+print(allboxes)
+
+quit()
 
 
 path = "/scratchf/flair_merged/test/D012_2019/IMG_Z10_UU.npy"
