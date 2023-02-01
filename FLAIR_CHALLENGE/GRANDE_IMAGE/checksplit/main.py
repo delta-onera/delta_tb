05cecee550c6e7f690a3c3aa9310836e4c037b10
achanhon <adrien.chan_hon_tong@onera.fr>
2023-02-01 13:46:37 +0100

    ++

--- FLAIR_CHALLENGE/GRANDE_IMAGE/checksplit/main.py
+++ FLAIR_CHALLENGE/GRANDE_IMAGE/checksplit/main.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision
+import numpy
 
-a = torch.load("/scratchf/flair_merged/test/D012_2019/IMG_Z10_UU.npy")
+a = numpy.load("/scratchf/flair_merged/test/D012_2019/IMG_Z10_UU.npy")
 print(a)
