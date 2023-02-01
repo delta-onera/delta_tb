35eb0ced0a645892f9d80aefa8f6a02ad58bb339
achanhon <adrien.chan_hon_tong@onera.fr>
2023-02-01 14:38:45 +0100

    ++

--- FLAIR_CHALLENGE/GRANDE_IMAGE/checksplit/main.py
+++ FLAIR_CHALLENGE/GRANDE_IMAGE/checksplit/main.py
@@ -8,5 +8,5 @@ a = numpy.load(path, allow_pickle=True)
 print(a)
 
 path = "/scratchf/CHALLENGE_IGN/test/D012_2019/Z10_UU/img/IMG_062700.tif"
-with rasterio.open(self.paths[i][0]) as src_img:
+with rasterio.open(path) as src_img:
     print(src_img.coords.BoundingBox)
