8f1fc89e75ec67c6a217629ad609e919f4190ece
achanhon <adrien.chan_hon_tong@onera.fr>
2023-02-01 13:52:39 +0100

    ++

--- FLAIR_CHALLENGE/GRANDE_IMAGE/checksplit/main.py
+++ FLAIR_CHALLENGE/GRANDE_IMAGE/checksplit/main.py
@@ -2,5 +2,7 @@ import torch
 import torchvision
 import numpy
 
-a = numpy.load("/scratchf/flair_merged/test/D012_2019/IMG_Z10_UU.npy")
+a = numpy.load(
+    "/scratchf/flair_merged/test/D012_2019/IMG_Z10_UU.npy", allow_pickle=True
+)
 print(a)
