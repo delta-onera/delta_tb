b1e49628e5a9216f69551f7f3faea2cdc54b6445
achanhon <adrien.chan_hon_tong@onera.fr>
2023-02-01 14:38:15 +0100

    ++

--- FLAIR_CHALLENGE/GRANDE_IMAGE/checksplit/main.py
+++ FLAIR_CHALLENGE/GRANDE_IMAGE/checksplit/main.py
@@ -1,8 +1,12 @@
 import torch
 import torchvision
 import numpy
+import rasterio
 
-a = numpy.load(
-    "/scratchf/flair_merged/test/D012_2019/IMG_Z10_UU.npy", allow_pickle=True
-)
+path = "/scratchf/flair_merged/test/D012_2019/IMG_Z10_UU.npy"
+a = numpy.load(path, allow_pickle=True)
 print(a)
+
+path = "/scratchf/CHALLENGE_IGN/test/D012_2019/Z10_UU/img/IMG_062700.tif"
+with rasterio.open(self.paths[i][0]) as src_img:
+    print(src_img.coords.BoundingBox)
