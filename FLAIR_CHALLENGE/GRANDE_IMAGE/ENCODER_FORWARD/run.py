43b74193332d4823c3140830acf7478fa87e9543
achanhon <adrien.chan_hon_tong@onera.fr>
2023-02-01 20:42:30 +0100

    ++

--- FLAIR_CHALLENGE/GRANDE_IMAGE/ENCODER_FORWARD/run.py
+++ FLAIR_CHALLENGE/GRANDE_IMAGE/ENCODER_FORWARD/run.py
@@ -4,7 +4,7 @@ import sys
 os.system("rm -rf __pycache__")
 os.system("rm -rf build")
 os.system("mkdir build")
-os.system("mv " + sys.argv[1] + "/build/model.pth build/model.pth")
+os.system("cp " + sys.argv[1] + "/build/model.pth build/model.pth")
 
 os.system("/d/achanhon/miniconda3/bin/python -u test.py")
 
