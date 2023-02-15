4ad5fe885a6d819555b31e4cec65ebdd14930b9b
achanhon <adrien.chan_hon_tong@onera.fr>
2023-02-15 14:54:02 +0100

    +++

--- FLAIR_CHALLENGE/GRANDE_IMAGE/END_TO_END/wtf.py
+++ FLAIR_CHALLENGE/GRANDE_IMAGE/END_TO_END/wtf.py
@@ -2,15 +2,16 @@ import torch
 import torchvision
 
 with torch.no_grad():
-    net1 = torchvision.models.efficientnet_v2_l(weights="DEFAULT").features
-    tmp = torch.cat([net1[0][0].weight.clone()] * 2, dim=1)
-
-    net2 = torchvision.models.efficientnet_v2_l(weights="DEFAULT").features
-    net2[0][0] = torch.nn.Conv2d(6, 32, kernel_size=3, stride=2, padding=1, bias=False)
-    net2[0][0].weight = torch.nn.Parameter(tmp * 0.5)
-
     a = torch.rand(2, 3, 256, 256)
+    net1 = torchvision.models.efficientnet_v2_l(weights="DEFAULT").features
+    net2 = torchvision.models.efficientnet_v2_l(weights="DEFAULT").features
+
+    print((net1(a) - net2(a)).abs().flatten().sum())
+
     b = torch.cat([a] * 2, dim=1)
+    net2[0][0] = torch.nn.Conv2d(6, 32, kernel_size=3, stride=2, padding=1, bias=False)
+    tmp = torch.cat([net1[0][0].weight.clone()] * 2, dim=1)
+    net2[0][0].weight = torch.nn.Parameter(tmp * 0.5)
 
     f1 = net1[0][0](a)
     f2 = net2[0][0](b)
