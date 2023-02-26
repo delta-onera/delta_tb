a3caaeb944e1e2604532a18da968266985005626
achanhon <adrien.chan_hon_tong@onera.fr>
2023-02-26 21:01:41 +0100

    +++

---
+++ FLAIR_CHALLENGE/TENTATIVE2/multiple/test.py
@@ -0,0 +1,40 @@
+import os
+import torch
+import torchvision
+import dataloader
+
+assert torch.cuda.is_available()
+
+print("load model")
+with torch.no_grad():
+    net = torch.load("build/model4.pth")
+    net = net.cuda()
+    net.eval()
+
+print("load data")
+dataset = dataloader.FLAIR("/scratchf/CHALLENGE_IGN/train/", "1/4")
+
+print("test")
+
+with torch.no_grad():
+    cm = torch.zeros((13, 13)).cuda()
+    for i in range(len(dataset.paths)):
+        if i % 100 == 99:
+            print(i, "/", len(dataset.paths))
+        x, y = dataset.getImageAndLabel(i)
+        x, y = x.cuda(), y.cuda()
+
+        z = net(x.unsqueeze(0))
+        _, z = z[0].max(0)
+        cm += dataloader.confusion(y, z)
+
+        if False:
+            debug = torch.stack([y, y, y], dim=0) / 13
+            torchvision.utils.save_image(debug, "build/" + str(i) + "_y.png")
+            debug = torch.stack([z, z, z], dim=0).float() / 13
+            torchvision.utils.save_image(debug, "build/" + str(i) + "_z.png")
+
+    print(cm)
+    print(dataloader.perf(cm))
+
+os._exit(0)
