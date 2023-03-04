a75c2986bc1cc0eee0a2ddf430517da9a91ca448
achanhon <adrien.chan_hon_tong@onera.fr>
2023-03-04 09:56:21 +0100

    ++++

--- FLAIR_CHALLENGE/TENTATIVE2/fusion/forward/run.py
+++ FLAIR_CHALLENGE/TENTATIVE2/fusion/forward/run.py
@@ -3,6 +3,6 @@ import sys
 
 os.system("rm -rf build")
 os.system("mkdir build")
-os.system("cp ../baseline/build/model.pth build/model.pth")
+os.system("cp ../train/build/model.pth build/model.pth")
 
 os.system("/d/achanhon/miniconda3/bin/python -u test.py")
