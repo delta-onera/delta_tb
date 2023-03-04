ec7457f3479143e9938c541fcc990ae8b0f2b8b9
achanhon <adrien.chan_hon_tong@onera.fr>
2023-03-04 09:50:46 +0100

    ++

--- FLAIR_CHALLENGE/TENTATIVE2/fusion/generatetest/dataloader.py
+++ FLAIR_CHALLENGE/TENTATIVE2/fusion/generatetest/dataloader.py
@@ -9,99 +9,9 @@ import threading
 import torchvision
 
 
-def confusion(y, z):
-    cm = torch.zeros(13, 13).cuda()
-    for a in range(13):
-        for b in range(13):
-            cm[a][b] = ((z == a).float() * (y == b).float()).sum()
-    return cm
-
-
-def perf(cm):
-    cmt = torch.transpose(cm, 0, 1)
-
-    accu = 0
-    for i in range(12):
-        accu += cm[i][i]
-    accu /= cm[0:12, 0:12].flatten().sum()
-
-    iou = 0
-    for i in range(12):
-        inter = cm[i][i]
-        union = cm[i].sum() + cmt[i].sum() - cm[i][i] + (cm[i][i] == 0)
-        iou += inter / union
-
-    return (iou / 12 * 100, accu * 100)
-
-
-def symetrie(x, y, ijk):
-    i, j, k = ijk[0], ijk[1], ijk[2]
-    if i == 1:
-        y = numpy.transpose(y, axes=(1, 0))
-        for u in range(x.shape[0]):
-            x[u] = numpy.transpose(x[u], axes=(1, 0))
-    if j == 1:
-        y = numpy.flip(y, axis=0)
-        for u in range(x.shape[0]):
-            x[u] = numpy.flip(x[u], axis=0)
-    if k == 1:
-        y = numpy.flip(y, axis=1)
-        for u in range(x.shape[0]):
-            x[u] = numpy.flip(x[u], axis=1)
-    return x.copy(), y.copy()
-
-
-class CropExtractor(threading.Thread):
-    def __init__(self, paths):
-        threading.Thread.__init__(self)
-        self.isrunning = False
-        self.maxsize = 161
-        self.paths = paths
-
-    def getName(self, i):
-        return self.paths[i][2]
-
-    def getImageAndLabel(self, i, torchformat=False):
-        with rasterio.open(self.paths[i][0]) as src_img:
-            x = src_img.read()
-            x = numpy.clip(numpy.nan_to_num(x), 0, 255)
-
-        y = PIL.Image.open(self.paths[i][1]).convert("L").copy()
-        y = numpy.asarray(y)
-        y = numpy.clip(numpy.nan_to_num(y) - 1, 0, 12)
-
-        if torchformat:
-            return torch.Tensor(x), torch.Tensor(y)
-        else:
-            return x, y
-
-    def getCrop(self):
-        assert self.isrunning
-        return self.q.get(block=True)
-
-    def getBatch(self, batchsize):
-        x = torch.zeros(batchsize, 5, 512, 512)
-        y = torch.zeros(batchsize, 512, 512)
-        for i in range(batchsize):
-            x[i], y[i] = self.getCrop()
-        return x, y
-
-    def run(self):
-        self.isrunning = True
-        self.q = queue.Queue(maxsize=self.maxsize)
-
-        while True:
-            i = int(torch.rand(1) * len(self.paths))
-            image, label = self.getImageAndLabel(i)
-            flag = numpy.random.randint(0, 2, size=3)
-            x, y = symetrie(image.copy(), label.copy(), flag)
-            x, y = torch.Tensor(x), torch.Tensor(y)
-            self.q.put((x, y), block=True)
-
-
 class FLAIR:
     def __init__(self, root, flag):
-        assert flag in ["1/4", "3/4", "1"]
+        assert flag in ["1"]
         self.root = root
         self.run = False
         self.paths = []
@@ -117,32 +27,18 @@ class FLAIR:
 
                 for name in names:
                     x = prefix + "/img/IMG_" + name
-                    y = prefix + "/msk/MSK_" + name
-                    self.paths.append((x, y, name))
+                    self.paths.append((x, name))
 
         self.paths = sorted(self.paths)
-        N = len(self.paths)
-        if flag == "3/4":
-            self.paths = [self.paths[i] for i in range(3 * N // 4)]
-        if flag == "1/4":
-            self.paths = [self.paths[i] for i in range(3 * N // 4, N)]
-
-        self.data = CropExtractor(self.paths)
 
     def getImageAndLabel(self, i):
-        return self.data.getImageAndLabel(i, torchformat=True)
+        with rasterio.open(self.paths[i][0]) as src_img:
+            x = src_img.read()
+            x = numpy.clip(numpy.nan_to_num(x), 0, 255)
+        return torch.Tensor(x)
 
     def getName(self, i):
-        return self.data.getName(i)
-
-    def getBatch(self, batchsize):
-        assert self.run
-        return self.data.getBatch(batchsize)
-
-    def start(self):
-        if not self.run:
-            self.run = True
-            self.data.start()
+        return self.paths[i][1]
 
 
 class UNET_EFFICIENTNET(torch.nn.Module):
