6fee0e65ae3e214074f164c6d310fe3d831bf7ee
Adrien Chan Hon Tong <you@example.com>
2023-03-14 08:41:51 +0100

    ++

--- FLAIR_CHALLENGE/APPROCHE_COMBINEE/FUSION/train.py
+++ FLAIR_CHALLENGE/APPROCHE_COMBINEE/FUSION/train.py
@@ -54,7 +54,7 @@ def diceloss(y, z):
 optimizer = torch.optim.Adam(net.parameters(), lr=0.00001)
 printloss = torch.zeros(1).cuda()
 stats = torch.zeros((13, 13)).cuda()
-batchsize = 5
+batchsize = 9
 nbbatchs = 100000
 dataset.start()
 
