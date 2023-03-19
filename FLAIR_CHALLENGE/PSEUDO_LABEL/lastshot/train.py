cb3a815c1a7caab8e743ad9f1756c76a7362d409
achanhon <adrien.chan_hon_tong@onera.fr>
2023-03-19 13:47:28 +0100

    ++

--- FLAIR_CHALLENGE/PSEUDO_LABEL/lastshot/train.py
+++ FLAIR_CHALLENGE/PSEUDO_LABEL/lastshot/train.py
@@ -55,7 +55,7 @@ def diceloss(y, z):
 optimizer = torch.optim.Adam(net.parameters(), lr=0.00001)
 printloss = torch.zeros(1).cuda()
 stats = torch.zeros((13, 13)).cuda()
-batchsize = 8
+batchsize = 5
 nbbatchs = 100000
 dataset.start()
 
