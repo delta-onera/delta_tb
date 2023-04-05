21ea330edc7f23515e0a6253b8f1e18186ea31b7
achanhon <adrien.chan_hon_tong@onera.fr>
2023-04-05 13:42:30 +0200

    ++

--- rarete/preprocessing/extractimagette.py
+++ rarete/preprocessing/extractimagette.py
@@ -3,7 +3,8 @@ import os
 root = "/scratchf/OSCD/"
 folders = os.listdir(root)
 folders = [f for f in folders if os.path.isdir(root + f)]
-folders = [f for f in folders if os.path.isdir(root + f + "/pair")]
-folders = [f for f in folders if os.path.isdir(root + f + "/cm")]
+folders = [f for f in folders if os.path.exists(root + f + "/pair/img1.png")]
+folders = [f for f in folders if os.path.exists(root + f + "/pair/img2.png")]
+folders = [f for f in folders if os.path.exists(root + f + "/cm/cm.png")]
 
 print(folders)
