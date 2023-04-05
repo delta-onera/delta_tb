54bc6a019ba61871c3cef73e188657d5cbadf3d1
achanhon <adrien.chan_hon_tong@onera.fr>
2023-04-05 17:31:27 +0200

    ++

--- rarete/baseline/run.py
+++ rarete/baseline/run.py
@@ -4,7 +4,7 @@ os.system("rm -rf __pycache__")
 os.system("rm -r build")
 os.system("mkdir build")
 
-os.system("python3 -u train.py")
-os.system("python3 -u test.py")
+os.system("/d/achanhon/miniconda3/python -u train.py")
+os.system("/d/achanhon/miniconda3/python3 -u test.py")
 
 os.system("rm -rf __pycache__")
