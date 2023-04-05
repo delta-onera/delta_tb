323c8cca46fe1855ba30e404790a480af91b705c
achanhon <adrien.chan_hon_tong@onera.fr>
2023-04-05 14:31:12 +0200

    ++

--- rarete/preprocessing/extractimagette.py
+++ rarete/preprocessing/extractimagette.py
@@ -15,6 +15,7 @@ print(folders)
 os.system("rm -r build")
 os.system("mkdir build")
 
+i = 0
 for path in folders:
     img1 = PIL.Image.open(root + path + "/pair/img1.png").convert("RGB").copy()
     img2 = PIL.Image.open(root + path + "/pair/img2.png").convert("RGB").copy()
@@ -25,3 +26,21 @@ for path in folders:
         print(path, img1.shape, img2.shape)
     if img1.shape[0:2] != cm.shape:
         print(path, img1.shape, cm.shape)
+
+    for row in range(0, cm.shape[0] - 33, 32):
+        for col in range(0, cm.shape[1] - 33, 32):
+            if cm[row : row + 32, col : col + 32].sum() > 0:
+                continue
+            crop1 = img1[row : row + 32, col : col + 32, :]
+            crop2 = img2[row : row + 32, col : col + 32, :]
+            crop1, crop2 = PIL.Image.fromarray(crop1), PIL.Image.fromarray(crop2)
+
+            crop1.save("build/" + str(i) + "_1.png")
+            crop2.save("build/" + str(i) + "_2.png")
+            i += 1
+
+            if i % 1000 == 999:
+                print(i, path)
+
+print("process terminated")
+print(i)
