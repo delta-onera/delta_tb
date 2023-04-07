19e3fc9250df47730e6570b65abc2018d99af08d
achanhon <adrien.chan_hon_tong@onera.fr>
2023-04-07 13:52:01 +0200

    ++

--- rarete/baseline/dataloader.py
+++ rarete/baseline/dataloader.py
@@ -11,7 +11,7 @@ import numpy
 def random_deformation(path, finalsize=256):
     translation_range = (0.1, 0.1)
     zoom_range = (0.9, 1.1)
-    roll_range, pitch_range, yaw_range = 30, 30, 30
+    roll_range, pitch_range, yaw_range = 60, 60, 60
 
     img = PIL.Image.open(path).convert("RGB").copy()
 
