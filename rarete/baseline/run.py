6e8cb85ddb1ac6ff0f97160dabad17dab0f7bb8c
achanhon <adrien.chan_hon_tong@onera.fr>
2023-04-18 17:24:29 +0200

    la deuxième tête a l'air de pas marcher du tout

--- rarete/baseline/run.py
+++ rarete/baseline/run.py
@@ -5,6 +5,7 @@ os.system("rm -r build")
 os.system("mkdir build")
 
 os.system("/d/achanhon/miniconda3/bin/python -u train.py")
+os.system("/d/achanhon/miniconda3/bin/python -u trainbis.py")
 os.system("/d/achanhon/miniconda3/bin/python -u test.py")
 
 os.system("rm -rf __pycache__")
--- rarete/baseline/train.py
+++ rarete/baseline/train.py
@@ -85,64 +85,6 @@ for i in range(nbbatchs):
     torch.nn.utils.clip_grad_norm_(net.parameters(), 3)
     optimizer.step()
 
-print("train second head")
-optimizer = torch.optim.Adam(net.p_.parameters(), lr=0.0001)
-printloss = 0
-CE = torch.nn.CrossEntropyLoss()
-nbbatchs = 1000
-for i in range(nbbatchs):
-    x1, x2, _ = dataset.getBatch()
-    with torch.no_grad():
-        z1, z2 = net(x1.cuda()), net(x2.cuda())
-        f1, f2 = net.f_(z1), net.f_(z2)
-        amers1 = torch.zeros(N, 16, 16).cuda()
-        amers2 = torch.zeros(N, 16, 16).cuda()
-
-    for n in range(N):
-        Z = z1[n].reshape(128, -1)
-        D = Z[:, :, None] - Z[:, None, :]
-        D = (D ** 2).mean(0)
-        for j in range(16):
-            D[j][j] = 10000
-            v, _ = D.min(1)
-            seuil = sorted(list(v))[-5]
-        amers1[n] = (v >= seuil).reshape(16, 16)
-
-        Z = z2[n].reshape(128, -1)
-        D = Z[:, :, None] - Z[:, None, :]
-        D = (D ** 2).mean(0)
-        for j in range(16):
-            D[j][j] = 10000
-            v, _ = D.min(1)
-            seuil = sorted(list(v))[-5]
-        amers2[n] = (v >= seuil).reshape(16, 16)
-
-    amers1, amers2 = amers1.long(), amers2.long()
-    p1, p2 = net.p_(z1), net.p_(z2)
-    loss = CE(p1, amers1) + CE(p2, amers2)
-
-    with torch.no_grad():
-        printloss += loss.clone().detach()
-
-        if i % 10 == 9:
-            print(i, printloss.cpu() / 10)
-            printloss = 0
-        if i % 100 == 99:
-            torch.save(net, "build/model.pth")
-
-    if i > nbbatchs * 0.1:
-        loss = loss * 0.5
-    if i > nbbatchs * 0.2:
-        loss = loss * 0.5
-    if i > nbbatchs * 0.5:
-        loss = loss * 0.5
-    if i > nbbatchs * 0.8:
-        loss = loss * 0.5
-
-    optimizer.zero_grad()
-    loss.backward()
-    torch.nn.utils.clip_grad_norm_(net.parameters(), 3)
-    optimizer.step()
-
 
 os._exit(0)
+
