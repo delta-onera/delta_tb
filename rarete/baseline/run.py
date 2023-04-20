e5de85a6881c05f1424340b27af79bafce8b7ee8
achanhon <adrien.chan_hon_tong@onera.fr>
2023-04-20 19:12:25 +0200

    ça marche vraiment pas le p -> mieux vaut faire la différence en fait

--- rarete/baseline/run.py
+++ rarete/baseline/run.py
@@ -5,7 +5,6 @@ os.system("rm -r build")
 os.system("mkdir build")
 
 os.system("/d/achanhon/miniconda3/bin/python -u train.py")
-os.system("/d/achanhon/miniconda3/bin/python -u trainbis.py")
 os.system("/d/achanhon/miniconda3/bin/python -u test.py")
 
 os.system("rm -rf __pycache__")
