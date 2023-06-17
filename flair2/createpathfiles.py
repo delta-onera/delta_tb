f43b922edc3962b235f8d47d75298cbc22691883
moi_moi_et_moi <adrien.chan_hon_tong@onera.fr>
2023-06-17 20:45:32 +0200

    ++

--- flair2/createpathfiles.py
+++ flair2/createpathfiles.py
@@ -15,18 +15,8 @@ root = "/scratchf/CHALLENGE_IGN/FLAIR_2/"
 
 with open(root + "flair-2_centroids_sp_to_patch.json") as fichier:
     coords = json.load(fichier)
-
-    tmp = min([v[0] for (_,v) in coords.items()])
-    print(tmp)
-    tmp = min([v[1] for (_,v) in coords.items()])
-    print(tmp)
-    tmp = max([v[0] for (_,v) in coords.items()])
-    print(tmp)
-    tmp = max([v[1] for (_,v) in coords.items()])
-    print(tmp)
-
-    coords = {key: coords[key] for key in sorted(coords.keys())}
-checkforget = set(coords.keys())
+    checkforget = set(coords.keys())
+    # coords = {key: coords[key] for key in sorted(coords.keys())}
 
 trainpaths = {}
 trainfolder, trainsubfolder = os.listdir(root + "flair_aerial_train/"), []
@@ -53,18 +43,22 @@ for tmp in coords:
             l = [s for s in l if "data.npy" in s]
             assert len(l) == 1
             trainpaths[num]["sen"] = tmp + l[0]
+            tmp = numpy.load(root + trainpaths[num]["sen"])
 
             a, b = coords["IMG_" + number6(num) + ".tif"]
-            assert 20 <= a <= 186 and 20 <= b <= 186
-            trainpaths[num]["coord"] = (a - 20, b - 20)
+            if 20 <= a <= tmp.shape[2] and 20 <= b <= tmp.shape[3]:
+                trainpaths[num]["coord"] = (a - 20, b - 20)
+            else:
+                print(a, b, tmp.shape)
+                quit()
 
 torch.save(trainpaths, root + "alltrainpaths.pth")
 
 print("THE SAME WITH TEST !!!!!!!!")
 trainpaths = {}
-trainfolder, trainsubfolder = os.listdir(root + "flair_2_aerial_test/"), []
+trainfolder, trainsubfolder = os.listdir(root + "flair_aerial_train/"), []
 for folder in trainfolder:
-    subfolder = os.listdir(root + "flair_2_aerial_test/" + folder)
+    subfolder = os.listdir(root + "flair_aerial_train/" + folder)
     trainsubfolder += [(folder + "/" + sub) for sub in subfolder]
 
 for tmp in coords:
@@ -83,10 +77,14 @@ for tmp in coords:
             l = [s for s in l if "data.npy" in s]
             assert len(l) == 1
             trainpaths[num]["sen"] = tmp + l[0]
+            tmp = numpy.load(root + trainpaths[num]["sen"])
 
             a, b = coords["IMG_" + number6(num) + ".tif"]
-            assert 20 <= a <= 186 and 20 <= b <= 186
-            trainpaths[num]["coord"] = (a - 20, b - 20)
+            if 20 <= a <= tmp.shape[2] and 20 <= b <= tmp.shape[3]:
+                trainpaths[num]["coord"] = (a - 20, b - 20)
+            else:
+                print(a, b, tmp.shape)
+                quit()
 
 torch.save(trainpaths, root + "alltestpaths.pth")
 
