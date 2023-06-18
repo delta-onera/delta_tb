e6b69162ccfe6209ce390f4044496ff84286ff48
moi_moi_et_moi <adrien.chan_hon_tong@onera.fr>
2023-06-18 10:47:51 +0200

    rahh

--- flair2/createpathfiles.py
+++ flair2/createpathfiles.py
@@ -16,8 +16,45 @@ root = "/scratchf/CHALLENGE_IGN/FLAIR_2/"
 with open(root + "flair-2_centroids_sp_to_patch.json") as fichier:
     coords = json.load(fichier)
     checkforget = set(coords.keys())
-    # coords = {key: coords[key] for key in sorted(coords.keys())}
 
+print("nb coords", len(coords))
+
+print("TEST")
+trainpaths = {}
+trainfolder, trainsubfolder = os.listdir(root + "flair_2_aerial_test/"), []
+for folder in trainfolder:
+    subfolder = os.listdir(root + "flair_2_aerial_test/" + folder)
+    trainsubfolder += [(folder + "/" + sub) for sub in subfolder]
+
+for tmp in coords:
+    num = int(tmp[4:-4])
+
+    for folder in trainsubfolder:
+        tmp = "flair_2_aerial_test/" + folder + "/img/IMG_"
+        if os.path.exists(root + tmp + number6(num) + ".tif"):
+            trainpaths[num] = {}
+            checkforget.remove("IMG_" + number6(num) + ".tif")
+            trainpaths[num]["image"] = tmp + number6(num) + ".tif"
+
+            tmp = "flair_2_sen_test/" + folder + "/sen/"
+            l = os.listdir(root + tmp)
+
+            l = [s for s in l if "data.npy" in s]
+            assert len(l) == 1
+            trainpaths[num]["sen"] = tmp + l[0]
+            tmp = numpy.load(root + trainpaths[num]["sen"])
+
+            a, b = coords["IMG_" + number6(num) + ".tif"]
+            if 20 <= a <= tmp.shape[2] and 20 <= b <= tmp.shape[3]:
+                trainpaths[num]["coord"] = (a - 20, b - 20)
+            else:
+                print(a, b, tmp.shape)
+                quit()
+
+print(len(trainpaths))
+torch.save(trainpaths, root + "alltestpaths.pth")
+
+print("TRAIN")
 trainpaths = {}
 trainfolder, trainsubfolder = os.listdir(root + "flair_aerial_train/"), []
 for folder in trainfolder:
@@ -53,39 +90,4 @@ for tmp in coords:
                 quit()
 
 torch.save(trainpaths, root + "alltrainpaths.pth")
-
-print("THE SAME WITH TEST !!!!!!!!")
-trainpaths = {}
-trainfolder, trainsubfolder = os.listdir(root + "flair_aerial_train/"), []
-for folder in trainfolder:
-    subfolder = os.listdir(root + "flair_aerial_train/" + folder)
-    trainsubfolder += [(folder + "/" + sub) for sub in subfolder]
-
-for tmp in coords:
-    num = int(tmp[4:-4])
-
-    for folder in trainsubfolder:
-        tmp = "flair_2_aerial_test/" + folder + "/img/IMG_"
-        if os.path.exists(root + tmp + number6(num) + ".tif"):
-            trainpaths[num] = {}
-            checkforget.remove("IMG_" + number6(num) + ".tif")
-            trainpaths[num]["image"] = tmp + number6(num) + ".tif"
-
-            tmp = "flair_2_sen_test/" + folder + "/sen/"
-            l = os.listdir(root + tmp)
-
-            l = [s for s in l if "data.npy" in s]
-            assert len(l) == 1
-            trainpaths[num]["sen"] = tmp + l[0]
-            tmp = numpy.load(root + trainpaths[num]["sen"])
-
-            a, b = coords["IMG_" + number6(num) + ".tif"]
-            if 20 <= a <= tmp.shape[2] and 20 <= b <= tmp.shape[3]:
-                trainpaths[num]["coord"] = (a - 20, b - 20)
-            else:
-                print(a, b, tmp.shape)
-                quit()
-
-torch.save(trainpaths, root + "alltestpaths.pth")
-
-print("checkforget", checkforget)
+print(len(trainpaths))
