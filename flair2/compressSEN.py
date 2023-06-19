6f0920e874d02e2dd5992b263cd74957e057a54e
moi_moi_et_moi <adrien.chan_hon_tong@onera.fr>
2023-06-19 21:15:42 +0200

    ++

--- flair2/compressSEN.py
+++ flair2/compressSEN.py
@@ -1,45 +1,33 @@
 import torch
 import numpy
+import os
 
 
 def compress(x):
-    print(x.flatten().max())
-    print(x.mean())
-    print(x.shape)
     xm = x.flatten(2).mean(2)
-    print(xm.shape)
     x_ = x - xm.unsqueeze(-1).unsqueeze(-1)
-    print(x_.shape)
     xv = torch.sqrt(((x_.flatten(2)) ** 2).mean(2))
     x_ = x_ / (xv.unsqueeze(-1).unsqueeze(-1) + 0.001)
     x_ = torch.clamp(x_, -4, 4)
     x__ = 4 * x / (x.flatten().max() + x.mean()) - 1
     x__ = torch.clamp(x__, -2, 2)
-    x = torch.cat([x_, x__], dim=1)
-    print(x.shape)
+    x = torch.cat([x_, x__], dim=1).half().float()
 
     tmp = x.flatten(1)
-    print(tmp.shape)
-    D = ((tmp.unsqueeze(0) - tmp.unsqueeze(1)) ** 2).mean(2)
-    print(D.shape)
+    D = torch.zeros(x.shape[0], x.shape[0]).cuda()
+    for i in range(x.shape[0]):
+        D[i] = ((tmp - tmp[i].unsqueeze(0)) ** 2).mean(dim=1)
 
-    T = [0, x.shape[-1]]
-    print(T)
+    T = [0, x.shape[0] - 1]
     for t in range(8):
         d = D[T].transpose(0, 1)
-        if t == 0:
-            print(d.shape)
         d, _ = d.min(1)
-        if t == 0:
-            print(d.shape)
         i = d.argmax()
-        if t == 0:
-            print(i)
-        T.append(i)
+        T = sorted(T + [int(i)])
 
     x = x[T]
-    assert x.shape[0:1] == (10, 20)
-    return x
+    assert x.shape[0:2] == (10, 20)
+    return x.half()
 
 
 root = "/scratchf/CHALLENGE_IGN/FLAIR_2/"
@@ -48,11 +36,14 @@ for name in l:
     paths = torch.load(root + name)
     print(len(paths))
     for i in paths:
+        os.system("ls -la " + root + paths[i]["sen"])
         sentinel = numpy.load(root + paths[i]["sen"])
 
         sentinel = torch.Tensor(numpy.float32(sentinel)).cuda()
         sentinel = compress(sentinel)
         sentinel = sentinel.cpu().numpy()
 
+        numpy.save("/d/achanhon/tmp", sentinel)
         # numpy.save(root + paths[i]["sen"], sentinel)
+        os.system("ls -la /d/achanhon/tmp.npy")
         quit()
