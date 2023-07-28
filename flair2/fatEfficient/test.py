17be0e1aa8c6d54f2edba6dd48f1529fb01d3049
moi_moi_et_moi <adrien.chan_hon_tong@onera.fr>
2023-07-28 21:18:05 +0200

    ++

---
+++ flair2/fatEfficient/test.py
@@ -0,0 +1,37 @@
+import torch
+import dataloader
+import numpy
+import PIL
+from PIL import Image
+
+assert torch.cuda.is_available()
+
+
+def number6(i):
+    s = str(i)
+    while len(s) < 6:
+        s = "0" + s
+    return s
+
+
+print("load model")
+net = torch.load("build/model.pth")
+net = net.cuda()
+net.eval()
+
+print("load data")
+dataset = dataloader.FLAIR2("test")
+
+print("test")
+stats = torch.zeros((13, 13)).cuda()
+with torch.no_grad():
+    for name in dataset.paths:
+        x, _ = dataset.get(name)
+        x = x.cuda()
+
+        z = net(x.unsqueeze(0))
+        _, z = z[0].max(0)
+
+        z = numpy.uint8(numpy.clip(z.cpu().numpy(), 0, 12))
+        z = PIL.Image.fromarray(z)
+        z.save("build/PRED_" + number6(name) + ".tif", compression="tiff_lzw")
