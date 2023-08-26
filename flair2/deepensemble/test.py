e7e4ba27894992e1436c63dd46c011a65e56dfbc
moi_moi_et_moi <adrien.chan_hon_tong@onera.fr>
2023-08-26 17:14:17 +0200

    ++

--- flair2/deepensemble/test.py
+++ flair2/deepensemble/test.py
@@ -9,6 +9,7 @@ torch.backends.cudnn.enabled = True
 torch.backends.cuda.matmul.allow_fp16_reduced_precision_reduction = True
 torch.backends.cudnn.benchmark = True
 
+
 def number6(i):
     s = str(i)
     while len(s) < 6:
@@ -16,10 +17,32 @@ def number6(i):
     return s
 
 
+class DeepEnsemble(torch.nn.Module):
+    def __init__(self, m1, m2, m3, m4):
+        super(DeepEnsemble, self).__init__()
+        self.m1 = torch.load(m1)
+        self.m2 = torch.load(m2)
+        self.m3 = torch.load(m3)
+        self.m4 = torch.load(m4)
+
+    def forward(self, x, s):
+        p1 = self.m1(x, s)
+        p2 = self.m2(x, s)
+        p3 = self.m3(x, s)
+        p4 = self.m4(x, s)
+        return p1 + 0.9 * p2 + p3 + p4
+
+
 print("load model")
-net = torch.load("build/model.pth")
+net = DeepEnsemble(
+    "../semisup2/build/model.pth",
+    "../semisup2bis/build/model.pth",
+    "../fast/build/model_converted.pth",
+    "../autrebacbone/build/model_converted.pth",
+)
 net = net.cuda()
 net.eval()
+net.half()
 
 print("load data")
 dataset = dataloader.FLAIR2("test")
@@ -31,7 +54,7 @@ with torch.no_grad():
         x, s = dataset.get(name)
         x, s = x.half().cuda(), s.half().cuda()
 
-        z = net(x.unsqueeze(0), s.unsqueeze(0), half=True)
+        z = net(x.unsqueeze(0), s.unsqueeze(0))
         _, z = z[0].max(0)
 
         z = numpy.uint8(numpy.clip(z.cpu().numpy(), 0, 12))
