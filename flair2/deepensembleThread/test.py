6658de455f1c0989722ccc825ef933b7d09a958c
moi_moi_et_moi <adrien.chan_hon_tong@onera.fr>
2023-09-01 10:37:31 +0200

    ++

--- flair2/deepensembleThread/test.py
+++ flair2/deepensembleThread/test.py
@@ -252,8 +252,8 @@ print("load model")
 net = DeepEnsemble(
     "../semisup2/build/model.pth",
     "../semisup2bis/build/model.pth",
-    "../fast/build/model_converted.pth",
-    "../fastbis/build/model_converted.pth",
+    "../vit/build/model_converted.pth",
+    "../vitbis/build/model_converted.pth",
     "../autrebacbone/build/model_converted.pth",
     "../autrebackbonebis/build/model_converted.pth",
 )
