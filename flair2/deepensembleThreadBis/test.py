2fb747632bef84695c2112a53e475f0ff1356cf5
moi_moi_et_moi <adrien.chan_hon_tong@onera.fr>
2023-09-18 19:58:54 +0200

    ++

--- flair2/deepensembleThreadBis/test.py
+++ flair2/deepensembleThreadBis/test.py
@@ -253,7 +253,7 @@ class DeepEnsemble(torch.nn.Module):
 
         p[:, 7, :, :] *= 1.125
         p[:, 9, :, :] *= 1.1
-        p[:, 10, :, :] *= 1.15
+        p[:, 10, :, :] *= 1.1
         return p
 
 
