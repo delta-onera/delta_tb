9c8474993eb239dfcaa834fd53535ce4a5b07cfc
moi_moi_et_moi <adrien.chan_hon_tong@onera.fr>
2024-02-08 11:47:00 +0100

    ++

--- geometry/firsttest/smaller.py
+++ geometry/firsttest/smaller.py
@@ -15,33 +15,34 @@ class FeatureExtractor(torch.nn.Module):
         self.features = tmp.cuda().half()
 
     def forward(self, x):
-        with torch.no_grad():
-            x = x.cuda().half() / 255.0
-            x = (x - 0.5) / 0.25
-            return self.features(x)
+        x = x.cuda().half() / 255.0
+        x = (x - 0.5) / 0.25
+        return self.features(x)
 
 
-net = FeatureExtractor()
-
-image = PIL.Image.open("/scratchf/DFC2015/BE_ORTHO_27032011_315130_56865.tif")
-image = numpy.asarray(image.convert("RGB").copy())
-image = torch.Tensor(image)[0:9920, 0:9920, :].clone()
-image = torch.stack([image[:, :, 0], image[:, :, 1], image[:, :, 2]], dim=0)
-image = image.unsqueeze(0)
-
-allfeatures = torch.zeros(128, 620, 620)
-allfeatures = torch.nn.functional.avg_pool2d(
-    allfeatures.unsqueeze(0), kernel_size=2, stride=2
-)
-allfeatures = allfeatures[0]
-for r in range(10):
-    for c in range(10):
-        x = image[:, :, 992 * r : 992 * (r + 1), 992 * c : 992 * (c + 1)]
-        z = net(x)[0].cpu()
-        allfeatures[:, 31 * r : 31 * (r + 1), 31 * c : 31 * (c + 1)] = z
-
-print("extract stats")
 with torch.no_grad():
+    net = FeatureExtractor()
+
+    image = PIL.Image.open("/scratchf/DFC2015/BE_ORTHO_27032011_315130_56865.tif")
+    image = numpy.asarray(image.convert("RGB").copy())
+    image = torch.Tensor(image)[0:9920, 0:9920, :].clone()
+    image = torch.stack([image[:, :, 0], image[:, :, 1], image[:, :, 2]], dim=0)
+    image = image.unsqueeze(0)
+
+    print("extract data")
+    allfeatures = torch.zeros(128, 620, 620)
+    for r in range(10):
+        for c in range(10):
+            x = image[:, :, 992 * r : 992 * (r + 1), 992 * c : 992 * (c + 1)]
+            z = net(x)[0].cpu()
+            allfeatures[:, 62 * r : 62 * (r + 1), 62 * c : 62 * (c + 1)] = z
+
+    allfeatures = torch.nn.functional.avg_pool2d(
+        allfeatures.unsqueeze(0), kernel_size=2, stride=2
+    )
+    allfeatures = allfeatures[0]
+
+    print("extract stats")
     allfeatures.cuda().half()
     norm = torch.sqrt((allfeatures**2).sum(0).unsqueeze(0)).half()
     allfeatures = allfeatures / (norm + 1)
