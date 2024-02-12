334105e9289fbaf37ababac5c23fa8151166cea7
moi_moi_et_moi <adrien.chan_hon_tong@onera.fr>
2024-02-12 18:49:45 +0100

    ++

--- geometry/v_alpha/main.py
+++ geometry/v_alpha/main.py
@@ -20,20 +20,23 @@ class FeatureExtractor(torch.nn.Module):
         return x
 
 
-def extractKeyCells(x, nb=300):
+def extractKeyCells(x, nb=300, seuil=None):
     assert len(x.shape) == 3
+    assert nb is None or seuil is None
+    assert nb is not None or seuil is not None
 
-    x = x / torch.sqrt((x * x + 0.1).sum(0).unsqueeze(0))
-    x = x.cuda().flatten(1)
+    z = x / torch.sqrt((x * x + 0.1).sum(0).unsqueeze(0))
+    z = z.cuda().flatten(1)
 
-    GRAM = torch.matmul(x.transpose(0, 1), x)
+    GRAM = torch.matmul(z.transpose(0, 1), z)
     GRAM = torch.nn.functional.softmax(GRAM, 1)
-    GRAM = torch.diagonal(GRAM)
+    GRAM = torch.diagonal(GRAM).cpu()
 
-    seuil = list(GRAM.cpu().numpy())
-    seuil = float(sorted(seuil)[-nb])
+    if seuil is None:
+        seuil = list(GRAM.numpy())
+        seuil = float(sorted(seuil)[-nb])
+
     GRAM = GRAM.view(x.shape[1], x.shape[2])
-
     return GRAM >= seuil
 
 
@@ -44,38 +47,41 @@ with torch.no_grad():
     image = numpy.asarray(image.convert("RGB").copy())
     image = torch.Tensor(image)[0:9600, 0:9600, :].clone()
     image = torch.stack([image[:, :, 0], image[:, :, 1], image[:, :, 2]], dim=0)
-    image = image.unsqueeze(0)
 
-    print("extract data")
-    allfeatures = torch.zeros(256, 150, 150)
+    print("extract feature on native image")
+    X = image.unsqueeze(0)
+    Z = torch.zeros(256, 150, 150)
     for r in range(10):
         for c in range(10):
-            x = image[:, :, 960 * r : 960 * (r + 1), 960 * c : 960 * (c + 1)]
+            x = X[:, :, 960 * r : 960 * (r + 1), 960 * c : 960 * (c + 1)]
+            z = net(x)[0]
+            Z[:, 15 * r : 15 * (r + 1), 15 * c : 15 * (c + 1)] = z
+
+    print("extract key point on native image")
+    keypoint_natif = extractKeyCells(Z)
+
+    print("extract feature on rotated image")
+    X = torch.rot90(image, dims=(1, 2)).unsqueeze(0)
+    Z = torch.zeros(256, 150, 150)
+    for r in range(10):
+        for c in range(10):
+            x = X[:, :, 960 * r : 960 * (r + 1), 960 * c : 960 * (c + 1)]
             z = net(x)[0].cpu()
-            allfeatures[:, 15 * r : 15 * (r + 1), 15 * c : 15 * (c + 1)] = z
+            Z[:, 15 * r : 15 * (r + 1), 15 * c : 15 * (c + 1)] = z
 
-    allfeatures = allfeatures / torch.sqrt(
-        (allfeatures * allfeatures + 0.1).sum(0).unsqueeze(0)
-    )
+    print("extract key point on rotated image")
+    keypoint = extractKeyCells(Z)
+    keypoint = torch.rot90(keypoint, k=-1)
 
-    print("extract stats")
-    allfeatures = allfeatures.cuda().flatten(1)
+    print("compare key point sets")
+    image = image.unsqueeze(0)
+    image = torch.nn.functional.interpolate(image, size=1200, mode="bilinear")[0]
+    tmp = keypoint.view(1, 1, 150, 150).float()
+    keypoint = torch.nn.functional.interpolate(tmp, size=1200, mode="bilinear")
+    tmp = keypoint_natif.view(1, 1, 150, 150).float()
+    keypoint_natif = torch.nn.functional.interpolate(tmp, size=1200, mode="bilinear")
 
-    GRAM = torch.matmul(allfeatures.transpose(0, 1), allfeatures)
-    del allfeatures
-    GRAM = torch.nn.functional.softmax(GRAM, 1)
-    GRAM = torch.diagonal(GRAM)
-    assert GRAM.shape[0] == (150 * 150)
-
-    seuil = list(GRAM.cpu().numpy())
-    seuil = float(sorted(seuil)[-300])
-    print(seuil)
-    GRAM = GRAM.view(150, 150).cpu()
-    print((GRAM >= seuil).float().sum())
-
-    imagetiny = torch.nn.functional.interpolate(image, size=150, mode="bilinear")
-    imagetiny = imagetiny[0] / 255
-    torchvision.utils.save_image(imagetiny, "build/image.png")
-
-    imagetiny *= (GRAM >= seuil).float().unsqueeze(0)
-    torchvision.utils.save_image(imagetiny, "build/amer.png")
+    image[1] = image[1] + ((keypoint > 0) * (keypoint_natif > 0)).float() * 50
+    image[0] = image[0] + ((keypoint > 0) != (keypoint_natif > 0)).float() * 50
+    image = torch.clamp(image, 0, 255)
+    torchvision.utils.save_image(image / 255, "build/amer.png")
